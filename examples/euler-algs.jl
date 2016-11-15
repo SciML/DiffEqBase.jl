0875f90f5783ed5b63e5727a09b1988e732111c7
Mauro Werder <mauro_lc@runbox.com>
2016-11-15 22:04:57 +0100

    tstops

--- examples/euler-algs.jl
+++ examples/euler-algs.jl
@@ -4,7 +4,7 @@
 using DiffEqBase
 
 # make a algorithm type
-abstract EulerAlgs <: OrdinaryDiffEqAlgorithm
+abstract EulerAlgs <: DiffEqBase.AbstractODEAlgorithm
 
 immutable FwdEulerAlg <: EulerAlgs
 end
@@ -14,55 +14,56 @@ end
 
 function solve{uType,tType,isinplace}(p::AbstractODEProblem{uType,tType,isinplace},
                                       Alg::Type{FwdEulerAlg};
-                                      tsteps=error("provide tsteps"))
+                                      tstops=error("provide tstops"))
     u0 = p.u0
     f = p.f
     tspan = p.tspan
-    @assert tsteps[1]==tspan[1]
-    @assert tsteps[end]==tspan[2]
+    @assert tstops[1]==tspan[1]
+    @assert tstops[end]==tspan[2]
     @assert !isinplace "Only out of place functions supported"
 
-    nt = length(tsteps)
+    nt = length(tstops)
     out = Vector{uType}(nt)
     out[1] = copy(u0)
     for i=2:nt
-        t = tsteps[i]
-        dt = t-tsteps[i-1]
+        t = tstops[i]
+        dt = t-tstops[i-1]
         out[i] = out[i-1] + dt*f(t,out[i-1])
     end
     # make solution type
-    build_ode_solution(p, Alg(), tsteps, out)
+    build_ode_solution(p, Alg(), tstops, out)
 end
 
 # Try it
 dt = 0.01
 p = ODETestProblem((t,u)->u, 1.0, (t,u) -> exp(t), (0.0,1.0))
-sol = solve(p, FwdEulerAlg, tsteps=0:dt:1)
+sol = solve(p, FwdEulerAlg, tstops=0:dt:1)
 
 function solve{uType,tType,isinplace}(p::AbstractODEProblem{uType,tType,isinplace},
                                       Alg::Type{BwdEulerAlg};
-                                      tsteps=error("provide tsteps"),
+                                      tstops=error("provide tstops"),
                                       tol=1e-5,
                                       maxiter=100)
     u0 = p.u0
     f = p.f
     tspan = p.tspan
-    @assert tsteps[1]==tspan[1]
-    @assert tsteps[end]==tspan[2]
+    @assert tstops[1]==tspan[1]
+    @assert tstops[end]==tspan[2]
+    # TODO: fix numparameters as it picks up the Jacobian
 #    @assert !isinplace "Only out of place functions supported"
     @assert DiffEqBase.has_jac(f) "Provide Jacobian as f(::Val{:jac}, ...)"
     jac = (t,u) -> f(Val{:jac}(), t, u)
 
-    nt = length(tsteps)
+    nt = length(tstops)
     out = Vector{uType}(nt)
     out[1] = copy(u0)
     for i=2:nt
-        t = tsteps[i]
-        dt = t-tsteps[i-1]
+        t = tstops[i]
+        dt = t-tstops[i-1]
         out[i] = newton(t, dt, out[i-1], f, jac, tol, maxiter)
     end
     # make solution type
-    build_ode_solution(p, Alg(), tsteps, out)
+    build_ode_solution(p, Alg(), tstops, out)
 end
 
 function newton(t, dt, u_last, f, jac, tol, maxiter)
@@ -80,15 +81,17 @@ end
 ff(t,u) = u
 ff(::Val{:jac}, t, u) = 1
 p2 = ODETestProblem(ff, 1.0, (t,u) -> exp(t), (0.0,1.0) )
-sol2 = solve(p2, BwdEulerAlg, tsteps=0:dt:1)
+sol2 = solve(p2, BwdEulerAlg, tstops=0:dt:1)
 
 
 
 using Plots
-plot(sol, plot_analytic=true)
+plot(sol)
 plot!(sol2)
+#plot!(sol2, plot_analytic=true)
 
+# using DiffEqDevTools
 # dts = 1./2.^(8:-1:4)
-# sim = test_convergence(dts,p,BwdEulerAlg)
+# sim = test_convergence(dts,p2,BwdEulerAlg)
 # plot(sim)
 # sim.𝒪est[:final]
