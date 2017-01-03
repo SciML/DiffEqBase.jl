4dde954e237d7f8f112c6344a0e22dedc1f635b3
ChrisRackauckas <me@chrisrackauckas.com>
2017-01-03 12:05:59 -0800

    expand integrator interface and refactor plotting

--- src/integrator_interface.jl
+++ src/integrator_interface.jl
@@ -7,6 +7,59 @@ get_proposed_dt(i::DEIntegrator) = error("This method has not been implemented f
 modify_proposed_dt!(i::DEIntegrator) = error("This method has not been implemented for the integrator")
 u_unmodified!(i::DEIntegrator,bool) = error("This method has not been implemented for the integrator")
 
-@recipe function f(i::DEIntegrator)
-  i.sol
+@recipe function f(integrator::DEIntegrator;plot_analytic=false,vars=nothing)
+
+  vars = interpret_vars(vars,integrator.sol)
+
+  dims = length(vars[1])
+  for var in vars
+    @assert length(var) == dims
+  end
+  # Should check that all have the same dims!
+
+
+  plot_vecs = []
+  for i in 1:dims
+    push!(plot_vecs,[])
+  end
+  @show vars
+  labels = String[]# Array{String, 2}(1, length(vars)*(1+plot_analytic))
+  for x in vars
+    for j in 1:dims
+      if x[j] == 0
+        push!(plot_vecs[j], integrator.t)
+      elseif x[j]==1 && !(typeof(integrator.u) <: AbstractArray)
+        push!(plot_vecs[j], integrator.u)
+      else
+        push!(plot_vecs[j], integrator.u[x[j]])
+      end
+    end
+    add_labels!(labels,x,dims,integrator.sol)
+  end
+
+  if plot_analytic
+    for x in vars
+      for j in 1:dims
+        if x[j] == 0
+          push!(plot_vecs[j], integrator.t)
+        elseif x[j]==1 && !(typeof(integrator.u) <: AbstractArray)
+          push!(plot_vecs[j], integrator.sol.prob.analytic(integrator.t,integrator.sol[1]))
+        else
+          push!(plot_vecs[j], integrator.sol.prob.analytic(integrator.t,integrator.sol[1])[x[j]])
+        end
+      end
+      add_labels!(labels,x,dims,integrator.sol)
+    end
+  end
+
+  xflip --> integrator.tdir < 0
+  seriestype --> :scatter
+  linewidth --> 3
+  #xtickfont --> font(11)
+  #ytickfont --> font(11)
+  #legendfont --> font(11)
+  #guidefont  --> font(11)
+  label --> reshape(labels,1,length(labels))
+  @show plot_vecs
+  (plot_vecs...)
 end
