292799e6d6c23ad5b6ead90c404127bfe1b6f4ef
ChrisRackauckas <me@chrisrackauckas.com>
2017-01-05 22:10:50 -0800

    tolerance in callbacks

--- src/callbacks.jl
+++ src/callbacks.jl
@@ -1,7 +1,16 @@
-immutable Callback{F1,F2} <: DECallback
+immutable Callback{F1,F2,T,T2} <: DECallback
   condition::F1
   affect!::F2
   rootfind::Bool
   interp_points::Int
   save_positions::Tuple{Bool,Bool}
+  abstol::T
+  reltol::T2
 end
+
+Callback(condition,affect!,
+         rootfind,interp_points,
+         save_positions;
+         abstol=0,reltol=1e-10) = Callback(condition,affect!,
+                    rootfind,interp_points,
+                    save_positions,abstol,reltol)
