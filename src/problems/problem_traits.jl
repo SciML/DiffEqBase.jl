e2dbd51e3540d0125735185b2c7f5099e0377989
ChrisRackauckas <chrisrackauckas@gmail.com>
2017-04-15 00:14:09 -0700

    add iip problem

--- src/problems/problem_traits.jl
+++ src/problems/problem_traits.jl
@@ -1,1 +1,3 @@
 is_diagonal_noise{uType,tType,isinplace,NoiseClass,ND}(prob::AbstractRODEProblem{uType,tType,isinplace,NoiseClass,ND}) = ND <: Void
+
+isinplace{uType,tType,iip}(prob::AbstractODEProblem{uType,tType,iip}) = iip
