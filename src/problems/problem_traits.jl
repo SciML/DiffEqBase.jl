18bae8d2ccc72583fce9be25b1e2e126fd9629fd
ChrisRackauckas <chrisrackauckas@gmail.com>
2017-05-07 11:46:22 -0700

    fix missing problem traits

--- src/problems/problem_traits.jl
+++ src/problems/problem_traits.jl
@@ -2,3 +2,6 @@ is_diagonal_noise{uType,tType,isinplace,ND}(prob::AbstractRODEProblem{uType,tTyp
 
 isinplace{uType,tType,iip}(prob::AbstractODEProblem{uType,tType,iip}) = iip
 isinplace{uType,iip}(prob::AbstractSteadyStateProblem{uType,iip}) = iip
+isinplace{uType,tType,iip,ND}(prob::AbstractRODEProblem{uType,tType,iip,ND}) = iip
+isinplace{uType,tType,lType,iip}(prob::AbstractDDEProblem{uType,tType,lType,iip}) = iip
+isinplace{uType,duType,tType,iip}(prob::AbstractDAEProblem{uType,duType,tType,iip}) = iip
