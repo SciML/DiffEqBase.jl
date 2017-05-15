0df3f7b4186aa6017f12f5b6e9891d9921eb4ff1
ChrisRackauckas <chrisrackauckas@gmail.com>
2017-05-14 23:01:23 -0700

    recursive norm

--- src/common_defaults.jl
+++ src/common_defaults.jl
@@ -1,5 +1,6 @@
 @inline UNITLESS_ABS2(x) = Real(abs2(x)/(typeof(x)(one(x))*typeof(x)(one(x))))
 @inline ODE_DEFAULT_NORM(u::AbstractArray) = sqrt(sum(UNITLESS_ABS2,u) / length(u))
+@inline ODE_DEFAULT_NORM{T<:AbstractArray,N}(u::AbstractArray{T,N}) = sqrt(sum(ODE_DEFAULT_NORM,u) / length(u))
 @inline ODE_DEFAULT_NORM(u) = norm(u)
 @inline ODE_DEFAULT_ISOUTOFDOMAIN(t,u) = false
 @inline ODE_DEFAULT_PROG_MESSAGE(dt,t,u) = "dt="*string(dt)*"\nt="*string(t)*"\nmax u="*string(maximum(abs.(u)))
