ae26b60ed22f4cbabb2f60f6c1af178fd1766032
David Widmann <dev+git@devmotion.de>
2017-07-28 01:27:42 +0200

    Change dot call behaviour

--- src/utils.jl
+++ src/utils.jl
@@ -29,15 +29,15 @@ end
 """
     @muladd ex
 
-Convert every combined multiplication and addition in `ex` into a call of `muladd`. If any
-of the involved operators or operands is dotted, `muladd` is applied as a "dot call".
+Convert every combined multiplication and addition in `ex` into a call of `muladd`. If both
+of the involved operators are dotted, `muladd` is applied as a "dot call".
 """
 macro muladd(ex)
   esc(to_muladd(ex))
 end
 
 function to_muladd(ex::Expr)
-    if !is_add_operation(ex)
+    if !isaddition(ex)
         if ex.head == :macrocall
             # expand macros first (enables use of @. inside of @muladd expression)
             return to_muladd(macroexpand(ex))
@@ -49,9 +49,16 @@ function to_muladd(ex::Expr)
 
     # retrieve summands of addition and split them into two groups, one with expressions
     # of multiplications and one with other expressions
+    # if addition is a dot call multiplications must be dot calls as well; if the addition
+    # is a regular operation only regular multiplications are filtered
     all_operands = to_muladd.(operands(ex))
-    mul_operands = filter(is_mul_operation, all_operands)
-    odd_operands = filter(x->!is_mul_operation(x), all_operands)
+    if isdotcall(ex)
+        mul_operands = filter(x->isdotcall(x, :*), all_operands)
+        odd_operands = filter(x->!isdotcall(x, :*), all_operands)
+    else
+        mul_operands = filter(x->isoperation(x, :*), all_operands)
+        odd_operands = filter(x->!isoperation(x, :*), all_operands)
+    end
 
     # define summands that are reduced with muladd and the initial element of the reduction
     if isempty(odd_operands)
@@ -68,7 +75,12 @@ function to_muladd(ex::Expr)
         if length(odd_operands) == 1
             last_operation = odd_operands[1]
         elseif isdotcall(ex)
-            last_operation = Expr(:., :+, Expr(:tuple, odd_operands...))
+            # make sure returned expression has same style as original expression
+            if ex.head == :.
+                last_operation = Expr(:., :+, Expr(:tuple, odd_operands...))
+            else
+                last_operation = Expr(:call, :.+, odd_operands...)
+            end
         else
             last_operation = Expr(:call, :+, odd_operands...)
         end
@@ -93,8 +105,8 @@ function to_muladd(ex::Expr)
             xs_factor2 = Expr(:call, :*, xs_operands[2:end]...)
         end
 
-        # create a dot call if any of the involved operators or operands is a dot call
-        if any(isdotcall, (ex, xs, xs_factor1, xs_factor2, r))
+        # create a dot call if both involved operators are dot calls
+        if isdotcall(ex)
             Expr(:., Base.muladd, Expr(:tuple, xs_factor1, xs_factor2, r))
         else
             Expr(:call, Base.muladd, xs_factor1, xs_factor2, r)
@@ -106,24 +118,33 @@ to_muladd(ex) = ex
 """
     isoperation(ex, op::Symbol)
 
-Determine whether `ex` is a call or "dot call" of operation `op`.
+Determine whether `ex` is a call of operation `op`.
 """
-isoperation(ex::Expr, op::Symbol) = !isempty(ex.args) &&
-    ((ex.head == :call && (ex.args[1] == op || ex.args[1] == Symbol('.', op))) ||
-     (ex.head == :. && ex.args[1] == op))
+isoperation(ex::Expr, op::Symbol) =
+    ex.head == :call && !isempty(ex.args) && ex.args[1] == op
 isoperation(ex, op::Symbol) = false
 
-is_add_operation(ex) = isoperation(ex, :+)
-is_mul_operation(ex) = isoperation(ex, :*)
-
 """
-    isdotcall(ex)
+    isdotcall(ex[, op])
 
-Determine whether `ex` is a dot call.
+Determine whether `ex` is a dot call and, in case `op` is specified, whether it calls
+operator `op`.
 """
-isdotcall(ex::Expr) = ex.head == :. || (ex.head == :call && !isempty(ex.args) &&
-                                        first(string(ex.args[1])) == '.')
+isdotcall(ex::Expr) = !isempty(ex.args) &&
+    (ex.head == :. ||
+     (ex.head == :call && !isempty(ex.args) && first(string(ex.args[1])) == '.'))
 isdotcall(ex) = false
+
+isdotcall(ex::Expr, op::Symbol) = isdotcall(ex) &&
+    (ex.args[1] == op || ex.args[1] == Symbol('.', op))
+isdotcall(ex, op::Symbol) = false
+
+"""
+    isaddition(ex)
+
+Determine whether `ex` is an expression of an addition.
+"""
+isaddition(ex) = isoperation(ex, :+) || isdotcall(ex, :+)
 
 """
     operands(ex)
