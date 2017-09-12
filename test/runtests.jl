3e1ea802702f035bf8e99321d6579feaad85aa3e
ChrisRackauckas <accounts@chrisrackauckas.com>
2017-09-12 12:56:10 -0700

    time the tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using DiffEqBase
 using Base.Test
 
+tic()
 @time @testset "Number of Parameters Calculation" begin include("numargs_test.jl") end
 @time @testset "Data Arrays" begin include("data_array_tests.jl") end
 @time @testset "Extended Functions" begin include("extended_function_tests.jl") end
@@ -9,3 +10,4 @@ using Base.Test
 @time @testset "Plot Variables" begin include("plot_vars.jl") end
 @time @testset "Problem Creation Tests" begin include("problem_creation_tests.jl") end
 @time @testset "Affine differential equation operators" begin include("affine_operators_tests.jl") end
+toc()
