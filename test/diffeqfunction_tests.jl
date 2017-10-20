cb6121c061df90737a672817ed3f78024de419a7
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2017-10-20 08:43:37 -0700

    fix test for export

--- test/diffeqfunction_tests.jl
+++ test/diffeqfunction_tests.jl
@@ -5,6 +5,6 @@ DE_f = DiffEqFunction{true}(f)
 t = 0.0; u = Float64[1,2,3]; du = zeros(3)
 DE_f(t,u,du)
 
-NS_f = DiffEqBase.NSODEFunction(f,t,u)
-NS_f = DiffEqBase.NSODEFunction{true}(f,t,u)
+NS_f = NSODEFunction(f,t,u)
+NS_f = NSODEFunction{true}(f,t,u)
 NS_f(t,u,du)
