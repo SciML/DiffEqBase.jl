76e6ed789af5494640f4688ee383402f0934f3d3
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2017-11-24 16:10:03 -0800

    add aux to analytical problem

--- src/problems/analytical_problems.jl
+++ src/problems/analytical_problems.jl
@@ -1,18 +1,19 @@
-struct AnalyticalProblem{uType,tType,isinplace,F,C} <: AbstractAnalyticalProblem{uType,tType,isinplace}
+struct AnalyticalProblem{uType,auxType,tType,isinplace,F,C} <: AbstractAnalyticalProblem{uType,tType,isinplace}
   f::F
   u0::uType
+  aux::auxType
   tspan::Tuple{tType,tType}
   callback::C
-  function AnalyticalProblem{iip}(f,u0,tspan;
+  function AnalyticalProblem{iip}(f,u0,aux,tspan;
            callback = nothing) where {iip}
-    new{typeof(u0),promote_type(map(typeof,tspan)...),iip,
-        typeof(f),typeof(callback)}(f,u0,tspan,callback)
+    new{typeof(u0),typeof(aux),promote_type(map(typeof,tspan)...),iip,
+        typeof(f),typeof(callback)}(f,u0,aux,tspan,callback)
   end
 end
 
-function AnalyticalProblem(f,u0,tspan;kwargs...)
+function AnalyticalProblem(f,u0,aux,tspan;kwargs...)
   iip = DiffEqBase.isinplace(f,6)
-  AnalyticalProblem{iip}(f,u0,tspan;kwargs...)
+  AnalyticalProblem{iip}(f,u0,aux,tspan;kwargs...)
 end
 
 export AnalyticalProblem, AbstractAnalyticalProblem
