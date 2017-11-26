cb539fae5bc9228efb7ff18398095d2821456b64
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2017-11-26 08:49:15 -0800

    linsolve initialization

--- src/linear_nonlinear.jl
+++ src/linear_nonlinear.jl
@@ -10,3 +10,6 @@ function (p::LinSolveFactorize)(x,A,b,update_matrix=false)
   A_ldiv_B!(x,p.A,b)
 end
 DEFAULT_LINSOLVE = LinSolveFactorize(lufact!)
+function (p::LinSolveFactorize)(::Type{Val{:init}},f,u0_prototype)
+  LinSolveFactorize(p.factorization,nothing)
+end
