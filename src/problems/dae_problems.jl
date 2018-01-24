7e799953b41053dc923aa67c1a23b6ffe09681ad
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-01-24 12:41:22 -0800

    fix construction syntax

--- src/problems/dae_problems.jl
+++ src/problems/dae_problems.jl
@@ -1,23 +1,23 @@
 # f(t,u,du,res) = 0
 struct DAEProblem{uType,duType,tType,isinplace,P,F,C,D} <: AbstractDAEProblem{uType,duType,tType,isinplace}
   f::F
-  u0::uType
   du0::duType
+  u0::uType  
   tspan::Tuple{tType,tType}
   p::P
   callback::C
   differential_vars::D
-  function DAEProblem{iip}(f,u0,du0,tspan,p=nothing;
+  function DAEProblem{iip}(f,du0,u0,tspan,p=nothing;
                       callback = nothing,
                       differential_vars = nothing) where {iip}
     new{typeof(u0),typeof(du0),promote_type(map(typeof,tspan)...),
                iip,typeof(p),typeof(f),typeof(callback),
                typeof(differential_vars)}(
-               f,u0,du0,tspan,p,callback,differential_vars)
+               f,du0,u0,tspan,p,callback,differential_vars)
   end
 end
 
-function DAEProblem(f,u0,du0,tspan,p=nothing;kwargs...)
+function DAEProblem(f,du0,u0,tspan,p=nothing;kwargs...)
   iip = typeof(f)<: Tuple ? isinplace(f[2],5) : isinplace(f,5)
-  DAEProblem{iip}(f,u0,du0,tspan,p;kwargs...)
+  DAEProblem{iip}(f,du0,u0,tspan,p;kwargs...)
 end
--- src/problems/dde_problems.jl
+++ src/problems/dde_problems.jl
@@ -1,18 +1,18 @@
 struct DDEProblem{uType,tType,lType,lType2,isinplace,P,F,H,C,MM} <:
                           AbstractDDEProblem{uType,tType,lType,isinplace}
   f::F
-  h::H
   u0::uType
-  constant_lags::lType
-  dependent_lags::lType2
+  h::H
   tspan::Tuple{tType,tType}
   p::P
+  constant_lags::lType
+  dependent_lags::lType2
   callback::C
   mass_matrix::MM
   neutral::Bool
-  function DDEProblem{isinplace}(f,h,u0,tspan,p=nothing,
-                                 constant_lags=nothing,
-                                 dependent_lags=nothing;
+  function DDEProblem{isinplace}(f,u0,h,tspan,p=nothing;
+                                 constant_lags=[],
+                                 dependent_lags=[],
                                  mass_matrix=I,
                                  neutral = mass_matrix == I ?
                                            false : det(mass_matrix)!=1,
@@ -20,15 +20,13 @@ struct DDEProblem{uType,tType,lType,lType2,isinplace,P,F,H,C,MM} <:
     new{typeof(u0),promote_type(map(typeof,tspan)...),
                typeof(constant_lags),typeof(dependent_lags),
                isinplace,typeof(p),typeof(f),typeof(h),typeof(callback),
-               typeof(mass_matrix)}(f,h,u0,constant_lags,
-                                    dependent_lags,tspan,p,callback,
+               typeof(mass_matrix)}(f,u0,h,tspan,p,constant_lags,
+                                    dependent_lags,callback,
                                     mass_matrix,neutral)
   end
 end
 
-function DDEProblem(f,h,u0,tspan,p=nothing,
-                    constant_lags=nothing,
-                    dependent_lags=nothing;kwargs...)
+function DDEProblem(f,u0,h,tspan,p=nothing;kwargs...)
   iip = typeof(f)<: Tuple ? isinplace(f[2],5) : isinplace(f,5)
-  DDEProblem{iip}(f,h,u0,tspan,p,constant_lags,dependent_lags;kwargs...)
+  DDEProblem{iip}(f,u0,h,tspan,p;kwargs...)
 end
