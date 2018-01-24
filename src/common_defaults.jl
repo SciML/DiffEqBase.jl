f1ac144a9aac387e479961a9bc87b451ee14200a
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-01-23 17:55:53 -0800

    fix unstable check dispatching

--- src/common_defaults.jl
+++ src/common_defaults.jl
@@ -9,8 +9,8 @@
 @inline ODE_DEFAULT_PROG_MESSAGE(dt,u,p,t) =
            "dt="*string(dt)*"\nt="*string(t)*"\nmax u="*string(maximum(abs.(u)))
 @inline ODE_DEFAULT_UNSTABLE_CHECK(dt,u,p,t) = false
-@inline ODE_DEFAULT_UNSTABLE_CHECK(dt,u,p,t::AbstractFloat) = isnan(u)
-@inline ODE_DEFAULT_UNSTABLE_CHECK(dt,u,p,t::AbstractArray{T}) where
+@inline ODE_DEFAULT_UNSTABLE_CHECK(dt,u::AbstractFloat,p,t) = isnan(u)
+@inline ODE_DEFAULT_UNSTABLE_CHECK(dt,u::AbstractArray{T},p,t) where
                                                {T<:AbstractFloat} = any(isnan,u)
-@inline ODE_DEFAULT_UNSTABLE_CHECK(dt,u,p,t::ArrayPartition) =
+@inline ODE_DEFAULT_UNSTABLE_CHECK(dt,u::ArrayPartition,p,t) =
                                                  any(any(isnan,x) for x in u.x)
