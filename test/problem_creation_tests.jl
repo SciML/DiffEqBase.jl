31cd1a476ac5ae5292d9013632385f7876a6d4f6
Takafumi Arakaki <aka.tkf@gmail.com>
2018-02-17 01:43:41 -0800

    Test problem constructors w/ @inferred (broken)

--- test/problem_creation_tests.jl
+++ test/problem_creation_tests.jl
@@ -15,6 +15,9 @@ prob = ODEProblem{true}(f,u0,tspan)
 prob = ODEProblem{false}(f,u0,tspan)
 @test isinplace(prob) == false
 
+@test_broken @inferred ODEProblem{true}(f,u0,tspan)
+@test_broken @inferred ODEProblem(f,u0,tspan)
+
 function f(dv,u,v,p,t)
   dv .= 2.*v
 end
@@ -36,12 +39,17 @@ tspan = (0,1.0)
 prob = SDEProblem(f,g,u0,tspan)
 prob = SDEProblem{true}(f,g,u0,tspan)
 
+@test_broken @inferred SDEProblem(f,g,u0,tspan)
+@test_broken @inferred SDEProblem{true}(f,g,u0,tspan)
+
 f_1delay = function (du,u,h,p,t)
     du[1] = - h(t-1)[1]
 end
 prob =  DDEProblem(f_1delay,ones(1),t->zeros(1),(0.0, 10.0),constant_lags = ones(1))
 prob =  DDEProblem{true}(f_1delay,ones(1),t->zeros(1),(0.0, 10.0),dependent_lags = ones(1))
 
+@test_broken @inferred DDEProblem(f_1delay,ones(1),t->zeros(1),(0.0, 10.0),constant_lags = ones(1))
+@test_broken @inferred DDEProblem{true}(f_1delay,ones(1),t->zeros(1),(0.0, 10.0),dependent_lags = ones(1))
 
 function f(r, yp, y, p,tres)
     r[1]  = -0.04*y[1] + 1.0e4*y[2]*y[3]
@@ -54,6 +62,8 @@ du0 = [-0.04, 0.04, 0.0]
 prob_dae_resrob = DAEProblem(f,du0,u0,(0.0,100000.0))
 prob_dae_resrob = DAEProblem{true}(f,du0,u0,(0.0,100000.0))
 
+@test_broken @inferred DAEProblem(f,du0,u0,(0.0,100000.0))
+@test_broken @inferred DAEProblem{true}(f,du0,u0,(0.0,100000.0))
 
 f(u,t,W) = 1.01u.+0.87u.*W
 u0 = 1.00
@@ -61,9 +71,15 @@ tspan = (0.0,1.0)
 prob = RODEProblem(f,u0,tspan)
 prob = RODEProblem{false}(f,u0,tspan)
 
+@test_broken @inferred RODEProblem(f,u0,tspan)
+@test_broken @inferred RODEProblem{false}(f,u0,tspan)
+
 DiscreteProblem(ones(1),tspan)
 f(t,u) = 0.5
 DiscreteProblem{false}(f,ones(1),tspan)
+
+@test_broken @inferred DiscreteProblem(ones(1),tspan)
+@test_broken @inferred DiscreteProblem{false}(f,ones(1),tspan)
 
 function f(du,u,t)
   du[1] = 2 - 2u[1]
@@ -71,3 +87,5 @@ function f(du,u,t)
 end
 u0 = zeros(2)
 prob = SteadyStateProblem(f,u0)
+
+@test_broken @inferred SteadyStateProblem(f,u0)
