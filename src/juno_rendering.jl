be8abcab011561335dfacacead7dfa1bc8871ddb
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-07-03 11:42:28 -0700

    add UUID

--- src/juno_rendering.jl
+++ src/juno_rendering.jl
@@ -1,4 +1,4 @@
-@require Juno begin
+@require Juno="e5e0dc1b-0480-54bc-9374-aad01c23163d" begin
   # Juno Rendering
   Juno.render(i::Juno.Inline, x::DESolution) = Juno.render(i, Juno.defaultrepr(x))
   Juno.render(i::Juno.Inline, x::DEIntegrator) = Juno.render(i, Juno.defaultrepr(x))
