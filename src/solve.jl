fd24b0b10e71f4d6b028d39d2145025963a0dc4f
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-07-05 01:10:34 -0700

    Handle IC distributions

--- src/solve.jl
+++ src/solve.jl
@@ -4,12 +4,20 @@ function solve! end
 
 function init(prob::DEProblem,args...;kwargs...)
   _prob = get_concrete_problem(prob,kwargs)
-  __init(_prob,alg,args...;kwargs...)
+  if haskey(kwargs,:alg)
+    __init(_prob,kwargs[:alg],args...;kwargs...)
+  else
+    __init(_prob,args...;kwargs...)
+  end
 end
 
 function solve(prob::DEProblem,args...;kwargs...)
   _prob = get_concrete_problem(prob,kwargs)
-  __solve(_prob,alg,args...;kwargs...)
+  if haskey(kwargs,:alg)
+    __solve(_prob,kwargs[:alg],args...;kwargs...)
+  else
+    __solve(_prob,args...;kwargs...)
+  end
 end
 
 function get_concrete_problem(prob,kwargs)
@@ -31,5 +39,12 @@ function get_concrete_problem(prob,kwargs)
     _u0 = prob.u0
   end
 
-  remake(prob;u0=_u0,tspan=_tspan)
+  __u0 = handle_distribution_u0(_u0)
+
+  remake(prob;u0=__u0,tspan=_tspan)
+end
+
+handle_distribution_u0(_u0) = _u0
+@require Distributions="31c24e10-a181-5473-b8eb-7969acd0382f" begin
+  handle_distribution_u0(_u0::Sampleable) = rand(_u0)
 end
