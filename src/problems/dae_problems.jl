104f8bae6d231c1159a7485a1b7d48a91c08b817
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-07-13 14:20:21 -0700

    update DAEs

--- src/problems/dae_problems.jl
+++ src/problems/dae_problems.jl
@@ -8,18 +8,19 @@ struct DAEProblem{uType,duType,tType,isinplace,P,J,F,C,D} <: AbstractDAEProblem{
   jac_prototype::J
   callback::C
   differential_vars::D
-  @add_kwonly function DAEProblem(f::AbstractODEFunction,du0,u0,tspan,p=nothing;
+  @add_kwonly function DAEProblem(f::AbstractDAEFunction,du0,u0,tspan,p=nothing;
                       jac_prototype = nothing,
                       callback = nothing,
                       differential_vars = nothing)
     _tspan = promote_tspan(tspan)
     new{typeof(u0),typeof(du0),typeof(_tspan),
-               iip,typeof(p),typeof(jac_prototype),
+               isinplace(f),typeof(p),typeof(jac_prototype),
                typeof(f),typeof(callback),
                typeof(differential_vars)}(
                f,du0,u0,_tspan,p,jac_prototype,
                callback,differential_vars)
   end
+
   function DAEProblem{iip}(f,du0,u0,tspan,p=nothing;kwargs...) where {iip}
     DAEProblem(convert(DAEFunction{iip},f),du0,u0,tspan,p;kwargs...)
   end
