003ef21c1c2b2184346e64c1ec4f73dea182abef
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-07-16 18:47:58 -0700

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 978512bbcd75044b0c0b582f560d020081a77459.

--- src/problems/sde_problems.jl
+++ src/problems/sde_problems.jl
@@ -12,40 +12,40 @@ struct SDEProblem{uType,tType,isinplace,P,J,NP,F,G,C,MM,ND} <: AbstractSDEProble
   mass_matrix::MM
   noise_rate_prototype::ND
   seed::UInt64
-end
-@add_kwonly function _SDEProblem(f::AbstractSDEFunction,g,u0,
-        tspan,p=nothing,problem_type=StandardSDEProblem();
-        jac_prototype = nothing,
-        noise_rate_prototype = nothing,
-        noise= nothing, seed = UInt64(0),
-        callback = nothing,mass_matrix=I)
-  _tspan = promote_tspan(tspan)
-  if mass_matrix == I && typeof(f) <: Tuple
-    _mm = ((I for i in 1:length(f))...,)
-  else
-    _mm = mass_matrix
+  @add_kwonly function SDEProblem(f::AbstractSDEFunction,g,u0,
+          tspan,p=nothing,problem_type=StandardSDEProblem();
+          jac_prototype = nothing,
+          noise_rate_prototype = nothing,
+          noise= nothing, seed = UInt64(0),
+          callback = nothing,mass_matrix=I)
+    _tspan = promote_tspan(tspan)
+    if mass_matrix == I && typeof(f) <: Tuple
+      _mm = ((I for i in 1:length(f))...,)
+    else
+      _mm = mass_matrix
+    end
+
+    new{typeof(u0),typeof(_tspan),
+        isinplace(f),typeof(p),typeof(jac_prototype),
+        typeof(noise),typeof(f),typeof(f.g),
+        typeof(callback),typeof(_mm),
+        typeof(noise_rate_prototype)}(
+        f,f.g,u0,_tspan,p,jac_prototype,
+        noise,callback,_mm,
+        noise_rate_prototype,seed)
   end
 
-  SDEProblem{typeof(u0),typeof(_tspan),
-      isinplace(f),typeof(p),typeof(jac_prototype),
-      typeof(noise),typeof(f),typeof(f.g),
-      typeof(callback),typeof(_mm),
-      typeof(noise_rate_prototype)}(
-      f,f.g,u0,_tspan,p,jac_prototype,
-      noise,callback,_mm,
-      noise_rate_prototype,seed)
-end
-
-function SDEProblem{iip}(f,g,u0,tspan,p=nothing;kwargs...) where {iip}
-  _SDEProblem(convert(SDEFunction{iip},f,g),g,u0,tspan,p;kwargs...)
+  function SDEProblem{iip}(f,g,u0,tspan,p=nothing;kwargs...) where {iip}
+    SDEProblem(convert(SDEFunction{iip},f,g),g,u0,tspan,p;kwargs...)
+  end
 end
 
 function SDEProblem(f::AbstractSDEFunction,u0,tspan,p=nothing;kwargs...)
-  _SDEProblem(f,f.g,u0,tspan,p;kwargs...)
+  SDEProblem(f,f.g,u0,tspan,p;kwargs...)
 end
 
 function SDEProblem(f,g,u0,tspan,p=nothing;kwargs...)
-  _SDEProblem(convert(SDEFunction,f,g),g,u0,tspan,p;kwargs...)
+  SDEProblem(convert(SDEFunction,f,g),g,u0,tspan,p;kwargs...)
 end
 
 abstract type AbstractSplitSDEProblem end
@@ -58,5 +58,5 @@ end
 function SplitSDEProblem{iip}(f1,f2,g,u0,tspan,p=nothing;
                                      func_cache=nothing,kwargs...) where iip
   iip ? _func_cache = similar(u0) : _func_cache = nothing
-  _SDEProblem{iip}(SplitFunction{iip}(f1,f2,_func_cache),g,u0,tspan,p,SplitSDEProblem{iip}();kwargs...)
+  SDEProblem{iip}(SplitFunction{iip}(f1,f2,_func_cache),g,u0,tspan,p,SplitSDEProblem{iip}();kwargs...)
 end
