b3f99b6acdcaccf922d83ba74f7d4285c452fe83
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-07-25 02:20:24 -0700

    better color?

--- src/utils.jl
+++ src/utils.jl
@@ -33,7 +33,7 @@ macro CSI_str(str)
     return :(string("\x1b[", $(esc(str)), "m"))
 end
 
-const TYPE_COLOR = CSI"32"
+const TYPE_COLOR = CSI"36"
 const NO_COLOR = CSI"0"
 
 macro def(name, definition)
