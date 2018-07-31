74668f6e46a5008f960c72a8f7d78855e2c23947
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-07-31 09:33:24 -0700

    make steady state problems have ODEFunctions

--- src/problems/steady_state_problems.jl
+++ src/problems/steady_state_problems.jl
@@ -3,15 +3,17 @@ struct SteadyStateProblem{uType,isinplace,P,F} <: AbstractSteadyStateProblem{uTy
   f::F
   u0::uType
   p::P
-  @add_kwonly function SteadyStateProblem{iip}(f,u0,p=nothing) where iip
-    new{typeof(u0),iip,typeof(p),
-        typeof(f)}(f,u0,p)
+  @add_kwonly function SteadyStateProblem(f::AbstractODEFunction,u0,p=nothing)
+    new{typeof(u0),isinplace(f),typeof(p),typeof(f)}(f,u0,p)
+  end
+
+  function SteadyStateProblem{iip}(f,u0,p=nothing) where iip
+    SteadyStateProblem(ODEFunction{iip}(f),u0,p)
   end
 end
 
 function SteadyStateProblem(f,u0,p=nothing;kwargs...)
-  iip = isinplace(f,3)
-  SteadyStateProblem{iip}(f,u0,p;kwargs...)
+  SteadyStateProblem(ODEFunction(f),u0,p;kwargs...)
 end
 
 SteadyStateProblem(prob::AbstractODEProblem) =
