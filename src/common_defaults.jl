bd5ad8d8034c30af25f506ee861d9bc027f332f9
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2019-02-07 09:22:16 -0500

    Add time to internalnorm and defaults

--- src/common_defaults.jl
+++ src/common_defaults.jl
@@ -1,10 +1,10 @@
 @inline UNITLESS_ABS2(x) = Real(abs2(x)/(typeof(x)(one(x))*typeof(x)(one(x))))
-@inline ODE_DEFAULT_NORM(u::Union{AbstractFloat,Complex}) = abs(u)
-@inline ODE_DEFAULT_NORM(u::Array{T}) where T<:Union{AbstractFloat,Complex} =
+@inline ODE_DEFAULT_NORM(u::Union{AbstractFloat,Complex},t) = abs(u)
+@inline ODE_DEFAULT_NORM(u::Array{T},t) where T<:Union{AbstractFloat,Complex} =
                                          @fastmath sqrt(sum(abs2,u) / length(u))
-@inline ODE_DEFAULT_NORM(u::AbstractArray) = sqrt(sum(UNITLESS_ABS2,u) / length(u))
-@inline ODE_DEFAULT_NORM(u::AbstractArray{T,N}) where {T<:AbstractArray,N} = sqrt(sum(ODE_DEFAULT_NORM,u) / length(u))
-@inline ODE_DEFAULT_NORM(u) = norm(u)
+@inline ODE_DEFAULT_NORM(u::AbstractArray,t) = sqrt(sum(UNITLESS_ABS2,u) / length(u))
+@inline ODE_DEFAULT_NORM(u::AbstractArray{T,N},t) where {T<:AbstractArray,N} = sqrt(sum(ODE_DEFAULT_NORM,u) / length(u))
+@inline ODE_DEFAULT_NORM(u,t) = norm(u)
 @inline ODE_DEFAULT_ISOUTOFDOMAIN(u,p,t) = false
 @inline ODE_DEFAULT_PROG_MESSAGE(dt,u,p,t) =
            "dt="*string(dt)*"\nt="*string(t)*"\nmax u="*string(maximum(abs.(u)))
--- src/init.jl
+++ src/init.jl
@@ -4,13 +4,23 @@ function __init__()
   end
 
   @require ForwardDiff="f6369f11-7733-5829-9624-2563aa707210" begin
-    @inline function ODE_DEFAULT_NORM(u::AbstractArray{<:ForwardDiff.Dual,N}) where {N}
+    # Support adaptive with non-dual time
+    @inline function ODE_DEFAULT_NORM(u::AbstractArray{<:ForwardDiff.Dual,N},t) where {N}
       sqrt(sum(ODE_DEFAULT_NORM,(ForwardDiff.value(x) for x in u)) / length(u))
     end
-    @inline function ODE_DEFAULT_NORM(u::Array{<:ForwardDiff.Dual,N}) where {N}
+    @inline function ODE_DEFAULT_NORM(u::Array{<:ForwardDiff.Dual,N},t) where {N}
       sqrt(sum(ODE_DEFAULT_NORM,(ForwardDiff.value(x) for x in u)) / length(u))
     end
-    @inline ODE_DEFAULT_NORM(u::ForwardDiff.Dual) = abs(ForwardDiff.value(u))
+    @inline ODE_DEFAULT_NORM(u::ForwardDiff.Dual,t) = abs(ForwardDiff.value(u))
+
+    # When time is dual, it shouldn't drop the duals for adaptivity
+    @inline function ODE_DEFAULT_NORM(u::AbstractArray{<:ForwardDiff.Dual,N},t::ForwardDiff.Dual) where {N}
+      sqrt(sum(ODE_DEFAULT_NORM,(x for x in u)) / length(u))
+    end
+    @inline function ODE_DEFAULT_NORM(u::Array{<:ForwardDiff.Dual,N},t::ForwardDiff.Dual) where {N}
+      sqrt(sum(ODE_DEFAULT_NORM,(x for x in u)) / length(u))
+    end
+    @inline ODE_DEFAULT_NORM(u::ForwardDiff.Dual,t::ForwardDiff.Dual) = abs(u)
 
     # Type piracy. Should upstream
     Base.nextfloat(d::ForwardDiff.Dual{T,V,N}) where {T,V,N} = ForwardDiff.Dual{T}(nextfloat(d.value), d.partials)
@@ -18,39 +28,55 @@ function __init__()
   end
 
   @require Measurements="eff96d63-e80a-5855-80a2-b1b0885c5ab7" begin
-    @inline function ODE_DEFAULT_NORM(u::AbstractArray{<:Measurements.Measurement,N}) where {N}
+    # Support adaptive steps should be errorless
+    @inline function ODE_DEFAULT_NORM(u::AbstractArray{<:Measurements.Measurement,N},t) where {N}
       sqrt(sum(ODE_DEFAULT_NORM,(Measurements.value(x) for x in u)) / length(u))
     end
-    @inline function ODE_DEFAULT_NORM(u::Array{<:Measurements.Measurement,N}) where {N}
+    @inline function ODE_DEFAULT_NORM(u::Array{<:Measurements.Measurement,N},t) where {N}
       sqrt(sum(ODE_DEFAULT_NORM,(Measurements.value(x) for x in u)) / length(u))
     end
-    @inline ODE_DEFAULT_NORM(u::Measurements.Measurement) = abs(Measurements.value(u))
+    @inline ODE_DEFAULT_NORM(u::Measurements.Measurement,t) = abs(Measurements.value(u))
   end
 
   @require Unitful="1986cc42-f94f-5a68-af5c-568840ba703d" begin
+    # Support adaptive errors should be errorless for exponentiation
     value(x::Unitful.Quantity) = x.val
-    @inline function ODE_DEFAULT_NORM(u::AbstractArray{<:Unitful.Quantity,N}) where {N}
+    @inline function ODE_DEFAULT_NORM(u::AbstractArray{<:Unitful.Quantity,N},t) where {N}
       sqrt(sum(ODE_DEFAULT_NORM,(value(x) for x in u)) / length(u))
     end
-    @inline function ODE_DEFAULT_NORM(u::Array{<:Unitful.Quantity,N}) where {N}
+    @inline function ODE_DEFAULT_NORM(u::Array{<:Unitful.Quantity,N},t) where {N}
       sqrt(sum(ODE_DEFAULT_NORM,(value(x) for x in u)) / length(u))
     end
-    @inline ODE_DEFAULT_NORM(u::Unitful.Quantity) = abs(value(u))
+    @inline ODE_DEFAULT_NORM(u::Unitful.Quantity,t) = abs(value(u))
   end
 
   @require Flux="587475ba-b771-5e3f-ad9e-33799f191a9c" begin
     value(x::Flux.Tracker.TrackedReal)  = x.data
     value(x::Flux.Tracker.TrackedArray) = x.data
-    @inline function ODE_DEFAULT_NORM(u::Flux.Tracker.TrackedArray) where {N}
+
+    # Support adaptive with non-tracked time
+    @inline function ODE_DEFAULT_NORM(u::Flux.Tracker.TrackedArray,t) where {N}
       sqrt(sum(ODE_DEFAULT_NORM,(value(x) for x in u)) / length(u))
     end
-    @inline function ODE_DEFAULT_NORM(u::AbstractArray{<:Flux.Tracker.TrackedReal,N}) where {N}
+    @inline function ODE_DEFAULT_NORM(u::AbstractArray{<:Flux.Tracker.TrackedReal,N},t) where {N}
       sqrt(sum(ODE_DEFAULT_NORM,(value(x) for x in u)) / length(u))
     end
-    @inline function ODE_DEFAULT_NORM(u::Array{<:Flux.Tracker.TrackedReal,N}) where {N}
+    @inline function ODE_DEFAULT_NORM(u::Array{<:Flux.Tracker.TrackedReal,N},t) where {N}
       sqrt(sum(ODE_DEFAULT_NORM,(value(x) for x in u)) / length(u))
     end
-    @inline ODE_DEFAULT_NORM(u::Flux.Tracker.TrackedReal) = abs(value(u))
+    @inline ODE_DEFAULT_NORM(u::Flux.Tracker.TrackedReal,t) = abs(value(u))
+
+    # Support TrackedReal time, don't drop tracking on the adaptivity there
+    @inline function ODE_DEFAULT_NORM(u::Flux.Tracker.TrackedArray,t::Flux.Tracker.TrackedReal) where {N}
+      sqrt(sum(ODE_DEFAULT_NORM,(value(x) for x in u)) / length(u))
+    end
+    @inline function ODE_DEFAULT_NORM(u::AbstractArray{<:Flux.Tracker.TrackedReal,N},t::Flux.Tracker.TrackedReal) where {N}
+      sqrt(sum(ODE_DEFAULT_NORM,(value(x) for x in u)) / length(u))
+    end
+    @inline function ODE_DEFAULT_NORM(u::Array{<:Flux.Tracker.TrackedReal,N},t::Flux.Tracker.TrackedReal) where {N}
+      sqrt(sum(ODE_DEFAULT_NORM,(value(x) for x in u)) / length(u))
+    end
+    @inline ODE_DEFAULT_NORM(u::Flux.Tracker.TrackedReal,t::Flux.Tracker.TrackedReal) = abs(value(u))
   end
 
 end
