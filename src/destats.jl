97a19f9ba9057bf756fc667243c19347516007f2
Chris Rackauckas <accounts@chrisrackauckas.com>
2019-03-08 18:22:08 -0500

    more informative defaults

--- src/destats.jl
+++ src/destats.jl
@@ -5,19 +5,23 @@ mutable struct DEStats
   njacs::Int
   nnonliniter::Int
   nnonlinconvfail::Int
-  nrootfind::Int
+  ncondition::Int
   naccept::Int
   nreject::Int
   maxeig::Float64
 end
 
-DEStats() = DEStats(ntuple(_->0, 5)..., 0.0)
+DEStats() = DEStats(ntuple(_->-1, 9)..., 0.0)
 
 function Base.show(io::IO, s::DEStats)
   println(io, summary(s))
   println(io, "Number of function evaluations: $(s.nf)")
   println(io, "Number of W matrix evaluations: $(s.nw)")
   println(io, "Number of linear solves:        $(s.nsolve)")
+  println(io, "Number of Jacobians created:        $(s.njacs)")
+  println(io, "Number of nonlinear solver iterations:        $(s.nnonliniter)")
+  println(io, "Number of nonlinear solver convergence failures:        $(s.nnonlinconvfail)")
+  println(io, "Number of rootfind condition calls:        $(s.ncondition)")
   println(io, "Number of accepted steps:       $(s.naccept)")
   print(io,   "Number of rejected steps:       $(s.nreject)")
   iszero(s.maxeig) || print(io, "\nMaximum eigenvalue recorded:    $(s.maxeig)")
