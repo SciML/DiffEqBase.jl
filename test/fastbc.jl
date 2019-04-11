d525275d11ae06493238ed346bf25b9292519e83
Yingbo Ma <mayingbo5@gmail.com>
2019-04-11 18:39:55 -0400

    Stronger tests

--- test/fastbc.jl
+++ test/fastbc.jl
@@ -12,7 +12,7 @@ function foo26(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v,
     @muladd @.. a = b + 0.1 * (0.2c + 0.3d + 0.4e + 0.5f + 0.6g + 0.6h + 0.6i + 0.6j + 0.6k + 0.6l + 0.6m + 0.6n + 0.6o + 0.6p + 0.6q + 0.6r + 0.6s + 0.6t + 0.6u + 0.6v + 0.6w + 0.6x + 0.6y + 0.6z)
     nothing
 end
-a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z = [rand(1000) for i in 1:26];
+a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z = [rand(100, 100, 2) for i in 1:26];
 @allocated foo9(a, b, c, d, e, f, g, h, i)
 @allocated foo26(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z)
 @test @allocated(foo9(a, b, c, d, e, f, g, h, i)) == 0
