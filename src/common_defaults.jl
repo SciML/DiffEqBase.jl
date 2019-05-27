5f5c1e95fa7cf97fc10c48bf4318a5c33a170a2d
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2019-05-26 21:37:23 -0400

    add an AbstractVectorOfArray overload

--- src/common_defaults.jl
+++ src/common_defaults.jl
@@ -3,6 +3,8 @@
 @inline ODE_DEFAULT_NORM(u::Union{AbstractFloat,Complex},t) = abs(u)
 @inline ODE_DEFAULT_NORM(u::Array{T},t) where T<:Union{AbstractFloat,Complex} =
                                          @fastmath sqrt(sum(abs2,u) / length(u))
+@inline ODE_DEFAULT_NORM(u::RecursiveArrayTools.AbstractVectorOfArray,t) =
+                                        sum(sqrt(sum(UNITLESS_ABS2,_u) / length(_u)) for _u in u.u)
 @inline ODE_DEFAULT_NORM(u::AbstractArray,t) = sqrt(sum(UNITLESS_ABS2,u) / length(u))
 @inline ODE_DEFAULT_NORM(u::AbstractArray{T,N},t) where {T<:AbstractArray,N} = sqrt(sum(x->ODE_DEFAULT_NORM(x[1],x[2]),zip(u,t)) / length(u))
 @inline ODE_DEFAULT_NORM(u,t) = norm(u)
