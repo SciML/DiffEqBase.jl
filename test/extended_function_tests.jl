6437c0d39a8cd6100a3907a5c25397bb39e7ceb0
Yingbo Ma <mayingbo5@gmail.com>
2019-06-21 20:47:56 -0400

    Fix tests

--- test/extended_function_tests.jl
+++ test/extended_function_tests.jl
@@ -1,25 +1,25 @@
 using Test, DiffEqBase
+using DiffEqBase: __has_jac, __has_tgrad, __has_invW, __has_invW_t, __has_paramderiv,
+  __has_paramjac, __has_syms, __has_analytic
 
-f123(x,y) = 1
-f123(::Val{:jac}, x) = 2
+struct Foo
+  jac
+  tgrad
+  invW
+  invW_t
+  deriv
+  paramjac
+  syms
+  analytic
+end
 
-g123(x,y) = 1
-g123(::Val{:jac}, x::T) where {T} = 2
+f = Foo(1,1,1,1,1,1,1,1)
 
-h123(x,y) = 1
-h123(x::T) where {T} = 2
-
-struct T123 end
-(::T123)(a) = 1
-(::T123)(::Val{:jac}, a) = 1
-t123 = T123()
-
-struct G123 end
-(::G123)(a) = 1
-(::G123)(b, a) = 1
-
-@test DiffEqBase.__has_jac(f123)
-@test DiffEqBase.__has_jac(g123)
-@test !DiffEqBase.__has_jac(h123)
-@test DiffEqBase.__has_jac(T123())
-@test !DiffEqBase.__has_jac(G123())
+@test __has_jac(f)
+@test __has_tgrad(f)
+@test __has_invW(f)
+@test __has_invW_t(f)
+@test __has_paramderiv(f)
+@test __has_paramjac(f)
+@test __has_syms(f)
+@test __has_analytic(f)
