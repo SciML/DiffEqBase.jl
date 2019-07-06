1cfedcfaa0dd7c003e4cfd6baf9977074c706d3c
Chris Rackauckas <accounts@chrisrackauckas.com>
2019-07-06 11:16:33 -0400

    fix extended function tests for invW change

--- test/extended_function_tests.jl
+++ test/extended_function_tests.jl
@@ -1,12 +1,12 @@
 using Test, DiffEqBase
-using DiffEqBase: __has_jac, __has_tgrad, __has_invW, __has_invW_t, __has_paramderiv,
+using DiffEqBase: __has_jac, __has_tgrad, __has_Wfact, __has_Wfact_t, __has_paramderiv,
   __has_paramjac, __has_syms, __has_analytic, __has_colorvec
 
 struct Foo
   jac
   tgrad
-  invW
-  invW_t
+  Wfact
+  Wfact_t
   deriv
   paramjac
   syms
@@ -18,8 +18,8 @@ f = Foo(1,1,1,1,1,1,1,1,1)
 
 @test __has_jac(f)
 @test __has_tgrad(f)
-@test __has_invW(f)
-@test __has_invW_t(f)
+@test __has_Wfact(f)
+@test __has_Wfact_t(f)
 @test __has_paramderiv(f)
 @test __has_paramjac(f)
 @test __has_syms(f)
