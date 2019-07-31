20fee60c85f6ce48c5497fc2efa81539783c7e53
Jared Lumpe <mjlumpe@gmail.com>
2019-07-31 16:38:46 -0700

    Refactor diffeqfunction_tests

--- test/diffeqfunction_tests.jl
+++ test/diffeqfunction_tests.jl
@@ -1,22 +1,64 @@
 using DiffEqBase, Test
 
-f_op = (u,p,t) -> u
-f_ip = (du,u,p,t) -> du .= u
-DE_f = ODEFunction{false}(f_op)
-DE_f_ip = ODEFunction{true}(f_ip)
-DI_f = DiscreteFunction{false}(f_op)
-DI_f_ip = DiscreteFunction{true}(f_ip)
-du = zeros(3); u = [1.0,2.0,3.0]; p = nothing; t = 0.0
-@test DE_f(u,p,t) == u
-DE_f_ip(du,u,p,t)
-@test du == u
-@test DI_f(u,p,t) == u
-DI_f_ip(du,u,p,t)
-@test du == u
+macro iop_def(funcdef::Expr)
+    """Define in- and out-of-place functions simultaneously.
 
-f(u,p,t) = u
+    Call on oop function definition, defines two functions with suffixes _op and _ip.
+    """
+    @assert funcdef.head ∈ (:function, :(=)) && funcdef.args[1].head == :call
+
+    fname = funcdef.args[1].args[1]
+
+    opname = Symbol("$(fname)_op")
+    ipname = Symbol("$(fname)_ip")
+
+    opdef = deepcopy(funcdef)
+    opdef.args[1].args[1] = opname
+
+    return quote
+        $(esc(opdef))
+        $(esc(ipname))(du, args...) = du .= $(esc(opname))(args...)
+    end
+end
+
+
+function test_inplace(du, expected, f::Function, args...)
+    """Test the in-place version of a function."""
+    fill!(du, NaN)
+    f(du, args...)
+    @test du == expected
+end
+
+# Allocate du automatically based on type of expected result
+test_inplace(expected, f::Function, args...) = test_inplace(similar(expected), expected, f, args...)
+
+function test_iop(expected, f_op::Function, f_ip::Function, args...)
+    """Test in- and out-of-place version of function both match expected value."""
+    @test f_op(args...) == expected
+    test_inplace(expected, f_ip, args...)
+end
+
+
+@iop_def f(u,p,t) = p[1] .* u
+u = [1.0, 2.0, 3.0]
+p = [2.0]
+t = 0.0
+
+
+# ODEFunction
+odefun = ODEFunction{false}(f_op)
+odefun_ip = ODEFunction{true}(f_ip)
+expected = f_op(u, p, t)
+test_iop(expected, odefun, odefun_ip, u, p, t)
+
+# DiscreteFunction
+dfun = DiscreteFunction{false}(f_op)
+dfun_ip = DiscreteFunction{true}(f_ip)
+test_iop(f_op(u, p, t), dfun, dfun_ip, u, p, t)
+
+
+# Type stability
+f_analytic(u,p,t) = u
 jac = (u,p,t) -> 1
 @inferred ODEFunction{false}(f_op,jac=jac)
-@inferred DiscreteFunction{false}(f_op,analytic=f)
-
-f(du,u,p,t) = u
+@inferred DiscreteFunction{false}(f_op,analytic=f_analytic)
