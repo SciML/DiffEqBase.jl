43263567d7481087d241a968a7afb20606ec357d
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2019-08-11 13:25:26 -0400

    Update init.jl

--- src/init.jl
+++ src/init.jl
@@ -2,7 +2,7 @@ value(x) = x
 cuify(x) = error("To use LinSolveGPUFactorize, you must do `using CuArrays`")
 
 # Piracy, should get upstreamed
-LinearAlgebra.ldiv!(Y::AbstractArray, A::AbstractArray, B::AbstractArray) = (copy!(Y,B); ldiv!(A,Y))
+LinearAlgebra.ldiv!(Y::AbstractArray, A::AbstractArray, B::AbstractArray) = (copyto!(Y,B); ldiv!(A,Y))
 
 function __init__()
   @require ApproxFun="28f2ccd6-bb30-5033-b560-165f7b14dc2f" begin
