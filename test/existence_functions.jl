531f18acb1b69cadfa02a5f2f64fe3a1326619e9
Jared Lumpe <mjlumpe@gmail.com>
2019-08-13 22:24:09 -0700

    Remove unused __has_paramderiv() function

--- test/existence_functions.jl
+++ test/existence_functions.jl
@@ -1,5 +1,5 @@
 using Test, DiffEqBase
-using DiffEqBase: __has_jac, __has_tgrad, __has_Wfact, __has_Wfact_t, __has_paramderiv,
+using DiffEqBase: __has_jac, __has_tgrad, __has_Wfact, __has_Wfact_t,
   __has_paramjac, __has_syms, __has_analytic, __has_colorvec,  has_jac, has_tgrad,
   has_Wfact, has_Wfact_t, has_paramjac, has_syms, has_analytic, has_colorvec,
   AbstractDiffEqFunction
@@ -9,20 +9,18 @@ struct Foo <: AbstractDiffEqFunction{false}
   tgrad
   Wfact
   Wfact_t
-  deriv
   paramjac
   syms
   analytic
   colorvec
 end
 
-f = Foo(1,1,1,1,1,1,1,1,1)
+f = Foo(1,1,1,1,1,1,1,1)
 
 @test __has_jac(f)
 @test __has_tgrad(f)
 @test __has_Wfact(f)
 @test __has_Wfact_t(f)
-@test __has_paramderiv(f)
 @test __has_paramjac(f)
 @test __has_syms(f)
 @test __has_analytic(f)
@@ -43,16 +41,14 @@ struct Foo2 <: AbstractDiffEqFunction{false}
   tgrad
   Wfact
   Wfact_t
-  deriv
 end
 
-f2 = Foo2(1,1,1,nothing,nothing)
+f2 = Foo2(1,1,nothing,nothing)
 
 @test __has_jac(f2)
 @test __has_tgrad(f2)
 @test __has_Wfact(f2)
 @test __has_Wfact_t(f2)
-@test __has_paramderiv(f2)
 @test !__has_paramjac(f2)
 @test !__has_syms(f2)
 @test !__has_analytic(f2)
@@ -60,7 +56,7 @@ f2 = Foo2(1,1,1,nothing,nothing)
 
 @test has_jac(f2)
 @test has_tgrad(f2)
-@test has_Wfact(f2)
+@test !has_Wfact(f2)
 @test !has_Wfact_t(f2)
 @test !has_paramjac(f2)
 @test !has_syms(f2)
