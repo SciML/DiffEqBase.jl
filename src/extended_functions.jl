793b4d57ca3b0b1fa79bc86c797798cd5a74c704
David Widmann <dev+github@devmotion.de>
2019-08-14 23:15:07 +0200

    Deprecate __has properties

--- src/extended_functions.jl
+++ src/extended_functions.jl
@@ -1,20 +1,20 @@
 # Standard
-__has_jac(f) = isdefined(f, :jac)
-__has_tgrad(f) = isdefined(f, :tgrad)
+@deprecate __has_jac(f) isdefined(f, :jac)
+@deprecate __has_tgrad(f) isdefined(f, :tgrad)
 
 # Performance
-__has_Wfact(f) = isdefined(f, :Wfact)
-__has_Wfact_t(f) = isdefined(f, :Wfact_t)
+@deprecate __has_Wfact(f) isdefined(f, :Wfact)
+@deprecate __has_Wfact_t(f) isdefined(f, :Wfact_t)
 
 # Parameter-Based
-__has_paramderiv(f) = isdefined(f, :deriv)
-__has_paramjac(f) = isdefined(f, :paramjac)
+@deprecate __has_paramderiv(f) isdefined(f, :deriv)
+@deprecate __has_paramjac(f) isdefined(f, :paramjac)
 
 ## Parameter Names Check
-__has_syms(f) = isdefined(f, :syms)
+@deprecate __has_syms(f) isdefined(f, :syms)
 
 ## Analytical Solution Check
-__has_analytic(f) = isdefined(f, :analytic)
+@deprecate __has_analytic(f) isdefined(f, :analytic)
 
 # Color vector for sparse Jacobian
-__has_colorvec(f) = isdefined(f,:colorvec)
+@deprecate __has_colorvec(f) isdefined(f,:colorvec)
