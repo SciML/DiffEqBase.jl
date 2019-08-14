98f7d797c61e120a29c8ba2d2067d60b5c6c7ac5
Jared Lumpe <mjlumpe@gmail.com>
2019-08-13 22:04:18 -0700

    Tests for has_{property}() functions

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,7 +9,7 @@ if GROUP == "All" || GROUP == "Core"
     @time @safetestset "Fast Broadcast" begin include("fastbc.jl") end
     @time @safetestset "Number of Parameters Calculation" begin include("numargs_test.jl") end
     @time @safetestset "Data Arrays" begin include("data_array_tests.jl") end
-    @time @safetestset "Extended Functions" begin include("extended_function_tests.jl") end
+    @time @safetestset "Existence functions" begin include("existence_functions.jl") end
     @time @safetestset "Callbacks" begin include("callbacks.jl") end
     @time @safetestset "Plot Variables" begin include("plot_vars.jl") end
     @time @safetestset "Problem Creation Tests" begin include("problem_creation_tests.jl") end
