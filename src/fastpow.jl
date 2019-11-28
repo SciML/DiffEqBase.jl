5c6e2ad77cb6cbacbc59d03190949767d8fbaa7e
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2019-11-28 04:40:39 -0500

    Fix fastpow zero limit

--- src/fastpow.jl
+++ src/fastpow.jl
@@ -83,5 +83,11 @@ end
 """
     fastpow(x::Real, y::Real) -> Float32
 """
-@inline fastpow(x::Real, y::Real) = _exp2(Float32(y) * fastlog2(convert(Float32, x)))
+@inline function fastpow(x::Real, y::Real) = 
+    if x == zero(x)
+        return 0f0
+    else
+        return _exp2(Float32(y) * fastlog2(convert(Float32, x)))
+    end
+end
 @inline fastpow(x, y) = x^y
