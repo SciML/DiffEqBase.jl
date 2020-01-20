8aa63acfe094ca4b82661125c73d57a3a1fa7fc0
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-01-20 16:45:11 -0500

    Revert "Simpler Tracker overloads"
    This reverts commit 407201fee0c4d431a507c0f4682106de348ff0c6.

--- src/init.jl
+++ src/init.jl
@@ -116,29 +116,28 @@ function __init__()
     value(x::Tracker.TrackedArray) = x.data
 
     @inline fastpow(x::Tracker.TrackedReal, y::Tracker.TrackedReal) = x^y
-    @inline Base.any(f::Function,x::Tracker.TrackedArray) = any(f,Tracker.data(x))
 
     # Support adaptive with non-tracked time
     @inline function ODE_DEFAULT_NORM(u::Tracker.TrackedArray,t) where {N}
-      sqrt(sum(abs2,Tracker.data(u)) / length(u))
+      sqrt(sum(x->ODE_DEFAULT_NORM(x[1],x[2]),zip((value(x) for x in u),Iterators.repeated(t))) / length(u))
     end
     @inline function ODE_DEFAULT_NORM(u::AbstractArray{<:Tracker.TrackedReal,N},t) where {N}
-      Tracker.data(sqrt(sum(abs2,u) / length(u)))
+      sqrt(sum(x->ODE_DEFAULT_NORM(x[1],x[2]),zip((value(x) for x in u),Iterators.repeated(t))) / length(u))
     end
     @inline function ODE_DEFAULT_NORM(u::Array{<:Tracker.TrackedReal,N},t) where {N}
-      Tracker.data(sqrt(sum(abs2,u) / length(u)))
+      sqrt(sum(x->ODE_DEFAULT_NORM(x[1],x[2]),zip((value(x) for x in u),Iterators.repeated(t))) / length(u))
     end
     @inline ODE_DEFAULT_NORM(u::Tracker.TrackedReal,t) = abs(value(u))
 
     # Support TrackedReal time, don't drop tracking on the adaptivity there
     @inline function ODE_DEFAULT_NORM(u::Tracker.TrackedArray,t::Tracker.TrackedReal) where {N}
-      sqrt(sum(abs2,u) / length(u))
+      sqrt(sum(x->ODE_DEFAULT_NORM(x[1],x[2]),zip(u,Iterators.repeated(t))) / length(u))
     end
     @inline function ODE_DEFAULT_NORM(u::AbstractArray{<:Tracker.TrackedReal,N},t::Tracker.TrackedReal) where {N}
-      sqrt(sum(abs2,u) / length(u))
+      sqrt(sum(x->ODE_DEFAULT_NORM(x[1],x[2]),zip(u,Iterators.repeated(t))) / length(u))
     end
     @inline function ODE_DEFAULT_NORM(u::Array{<:Tracker.TrackedReal,N},t::Tracker.TrackedReal) where {N}
-      sqrt(sum(abs2,u) / length(u))
+      sqrt(sum(x->ODE_DEFAULT_NORM(x[1],x[2]),zip(u,Iterators.repeated(t))) / length(u))
     end
     @inline ODE_DEFAULT_NORM(u::Tracker.TrackedReal,t::Tracker.TrackedReal) = abs(u)
   end
