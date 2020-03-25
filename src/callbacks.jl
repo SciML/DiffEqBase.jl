2300b0d24bbb13b6ad9f3ba646a813bd5e1bb88b
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-03-25 11:36:56 -0400

    handle nothing callbacks

--- src/callbacks.jl
+++ src/callbacks.jl
@@ -324,6 +324,7 @@ Base.isempty(cb::AbstractDiscreteCallback) = false
 has_continuous_callback(cb::DiscreteCallback) = false
 has_continuous_callback(cb::ContinuousCallback) = true
 has_continuous_callback(cb::CallbackSet) = !isempty(cb.continuous_callbacks)
+has_continuous_callback(cb::Nothing) = false
 
 #======================================================#
 # Callback handling
