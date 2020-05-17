f5c4270fdd58269bb8684ca42935d0d8d2308562
Yingbo Ma <mayingbo5@gmail.com>
2020-05-17 04:42:24 -0400

    Return an array of solutions

--- src/ensemble/basic_ensemble_solve.jl
+++ src/ensemble/basic_ensemble_solve.jl
@@ -95,7 +95,7 @@ function __solve(prob::AbstractEnsembleProblem,
   if num_batches == 1 && prob.reduction === DEFAULT_REDUCTION
     elapsed_time = @elapsed u = batch_function(1:trajectories)
     _u = tighten_container_eltype(u)
-    return EnsembleSolution([_u],elapsed_time,true)
+    return EnsembleSolution(_u,elapsed_time,true)
   end
 
   if prob.u_init === nothing && prob.reduction === DEFAULT_REDUCTION
@@ -118,6 +118,7 @@ function __solve(prob::AbstractEnsembleProblem,
     converged && break
   end
 
+  u = reduce(vcat, u)
   _u = tighten_container_eltype(u)
 
   return EnsembleSolution(u,elapsed_time,converged)
