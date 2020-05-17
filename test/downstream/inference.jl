ddc68d90f6ceb6147e3da07649cff99124f0ddc0
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-05-17 04:54:40 -0400

    add an ensemble inference test

--- test/downstream/inference.jl
+++ test/downstream/inference.jl
@@ -9,3 +9,38 @@ tspan = (0.0,1.0)
 prob = ODEProblem(lorenz,u0,tspan)
 sol = solve(prob,Tsit5())
 @inferred solve(prob,Tsit5())
+
+function f(du,u,p,t)
+  du[1] = p.a
+  du[2] = p.b
+end
+
+const alg = Tsit5()
+
+function solve_ode(f::F, p::P) where {F,P}
+
+  tspan = (0., 1.0)
+  Δt = tspan[2] - tspan[1]
+  dt = 1/252
+  nodes = Int(ceil(Δt / dt) + 1)
+  t = T = [tspan[1] + (i - 1) * dt for i = 1:nodes]
+
+  # if I do not set {true}, prob type Any...
+  prob = ODEProblem{true}(f, [0., 0.], tspan, p)
+  # prob = ODEProblem(f, [0., 0.], tspan, p)
+
+  prob_func = (prob, i, repeat) -> begin
+    remake(prob, tspan = (T[i + 1], t[1]))
+  end
+
+  # ensemble problem
+  odes = EnsembleProblem(prob, prob_func = prob_func)
+
+  sol = OrdinaryDiffEq.solve(
+    odes, OrdinaryDiffEq.Tsit5(), OrdinaryDiffEq.EnsembleThreads(),
+    trajectories = nodes - 1, saveat = -dt
+  )
+
+  return sol
+end
+@inferred solve_ode(f, (a = 1, b = 1))
