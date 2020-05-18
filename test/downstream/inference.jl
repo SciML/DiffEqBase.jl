53987dd8a6a025172fdad16afb936bd74cb69288
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-05-17 21:26:17 -0400

    fix clashing tests

--- test/downstream/inference.jl
+++ test/downstream/inference.jl
@@ -47,10 +47,10 @@ end
 
 using StochasticDiffEq, Test
 u0=1/2
-f(u,p,t) = u
-g(u,p,t) = u
+ff(u,p,t) = u
+gg(u,p,t) = u
 dt = 1//2^(4)
 tspan = (0.0,1.0)
-prob = SDEProblem(f,g,u0,(0.0,1.0))
+prob = SDEProblem(ff,gg,u0,(0.0,1.0))
 sol = solve(prob,EM(),dt=dt)
 @inferred solve(prob,EM(),dt=dt)
