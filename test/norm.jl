196613e7aef2a5e2038cd4cc3b9260647121ecfa
Yingbo Ma <mayingbo5@gmail.com>
2020-06-16 21:53:15 -0400

    Relax norm test (floating point numbers are really weird)

--- test/norm.jl
+++ test/norm.jl
@@ -7,9 +7,9 @@ const internalnorm = ODE_DEFAULT_NORM
 val = rand(10)
 par = rand(10)
 u = Dual.(val, par)
-reference = sqrt((sum(abs2,val) + sum(abs2,par)) / (length(val) + length(par)))
+reference(val, par) = sqrt((sum(abs2,val) + sum(abs2,par)) / (length(val) + length(par)))
 dual_real = internalnorm(u, 1)
 dual_dual = internalnorm(u, u[1])
-@test reference === dual_real
-@test reference == dual_dual
+@test reference(val, par) ≈ dual_real
+@test reference(val, par) ≈ dual_dual
 @test partials(dual_dual, 1) ≈ gradient(x->internalnorm(x, x[1]), val)'par
