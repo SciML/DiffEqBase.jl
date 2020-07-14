439cd80afdde717581eab543f1e5deb309b07366
Yingbo Ma <mayingbo5@gmail.com>
2020-07-14 16:26:53 -0400

    Pretty printing

--- src/tableaus.jl
+++ src/tableaus.jl
@@ -1,7 +1,7 @@
 """
-$(TYPEDEF)
+  $(TYPEDEF)
 
-Holds a tableau which defines an explicit Runge-Kutta method.
+Holds a tableau which defines a Runge-Kutta method.
 """
 struct RKTableau{AType,bType,bbType,cType,
                  fsal,explicit,stages,order,adaptiveorder,name} <: ODERKTableau
@@ -15,16 +15,18 @@ function RKTableau(A, c, b, order; adaptiveorder=0, bEEst=nothing, name=:RK, kwa
   eeststages = bEEst === nothing ? stages : length(bEEst)
   size(A, 1) == size(A, 2) == stages == eeststages || error("The size of A is not consistent with b or embedded b.")
   isfsal = iszero(c[1]) && isone(c[stages]) && A[stages, :] == b
-  isexplicit = true
-  for j in axes(A, 2), i in j:stages
-    isexplicit &= iszero(A[i, j])
-  end
+  isexplicit = istril(A, -1)
 
   RKTableau{typeof(A), typeof(b), typeof(bEEst), typeof(c),
             isfsal, isexplicit, stages, order, adaptiveorder, name}(
     A, c, b, bEEst
   )
 end
+
+
+###
+### Interface
+###
 
 const ExplicitRKTableau{AType,bType,bbType,cType,fsal,stages,order,adaptiveorder} = RKTableau{AType,bType,bbType,cType,fsal,true,stages,order,adaptiveorder}
 const ImplicitRKTableau{AType,bType,bbType,cType,fsal,stages,order,adaptiveorder} = RKTableau{AType,bType,bbType,cType,fsal,false,stages,order,adaptiveorder}
@@ -45,7 +47,53 @@ get_tableau_A(tab::ODERKTableau) = tab.A
 get_tableau_b(tab::ODERKTableau) = tab.b
 get_tableau_bEEst(tab::ODERKTableau) = tab.bEEst
 get_tableau_c(tab::ODERKTableau) = tab.c
+get_tableau_name(tab::RKTableau{AType,bType,bbType,cType,fsal,explicit,stages,order,adaptiveorder,name}) where {AType,bType,bbType,cType,fsal,explicit,stages,order,adaptiveorder,name} = name
 
-for f in [:isexplicit, :alg_order, :alg_adaptive_order, :isadaptive, :isfsal, :alg_stages, :get_tableau_A, :get_tableau_b, :get_tableau_bEEst, :get_tableau_c]
+for f in [:isexplicit, :alg_order, :alg_adaptive_order, :isadaptive, :isfsal, :alg_stages, :get_tableau_A, :get_tableau_b, :get_tableau_bEEst, :get_tableau_c, :get_tableau_name]
   @eval $f(::T) where {T<:Any} = error("`$f(::$T)` is not defined.")
+end
+
+###
+### Pretty printing
+###
+
+function algstats(tab::ODERKTableau)
+  stats = String[]
+  push!(stats, isexplicit(tab) ? "explicit" : "implicit")
+  isfsal(tab) && push!(stats, "FSAL")
+  stages = alg_stages(tab)
+  push!(stats, "stages=$stages")
+  order = alg_order(tab)
+  push!(stats, "order=$order")
+  if isadaptive(tab)
+    adaptiveorder = alg_adaptive_order(tab)
+    push!(stats, "embedded order=$adaptiveorder")
+  end
+  return join(stats, ", ")
+end
+
+struct ExplicitA{T} <: AbstractMatrix{T}
+  data::AbstractMatrix{T}
+end
+Base.getindex(A::ExplicitA, args...) = getindex(A.data, args...)
+Base.size(A::ExplicitA, args...) = size(A.data, args...)
+
+Base.replace_in_print_matrix(A::ExplicitA, i::Integer, j::Integer, s::AbstractString) = i > j ? s : Base.replace_with_centered_mark(s)
+
+function Base.show(io::IO, tab::ODERKTableau)
+  println(io, get_tableau_name(tab),
+          "{",
+          algstats(tab),
+          "}",
+          ":\n", "A:")
+  A = get_tableau_A(tab)
+  Base.print_matrix(io, isexplicit(tab) ? ExplicitA(A) : A)
+  println(io, "\nb:")
+  Base.print_matrix(io, get_tableau_b(tab)')
+  if isadaptive(tab)
+    println(io, "\nembedded b:")
+    Base.print_matrix(io, get_tableau_bEEst(tab)')
+  end
+  println(io, "\nc:")
+  Base.print_matrix(io, get_tableau_c(tab))
 end
