7e6faa3999aa15fc1bf3ea4b0be75d25585ca9f7
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2020-09-02 02:10:58 -0400

    Guard against invalid DAEProblems with non-uniform dimensionality between u0, du0, and differential_vars. Should prevent undefined memory access in Sundials.jl when invalid inputs are passed.
    https://github.com/SciML/DiffEqBase.jl/issues/581

--- src/problems/dae_problems.jl
+++ src/problems/dae_problems.jl
@@ -14,6 +14,11 @@ struct DAEProblem{uType,duType,tType,isinplace,P,F,K,D} <: AbstractDAEProblem{uT
                       du0,u0,tspan,p=NullParameters();
                       differential_vars = nothing,
                       kwargs...) where {iip}
+    # Defend against external solvers like Sundials breaking on non-uniform input dimensions.
+    size(du0) == size(u0) || throw(ArgumentError("Sizes of u0 and du0 must be the same."))
+    if !isnothing(differential_vars)
+        size(u0) == size(differential_vars) || throw(ArgumentError("Sizes of u0 and differential_vars must be the same."))
+    end
     _tspan = promote_tspan(tspan)
     new{typeof(u0),typeof(du0),typeof(_tspan),
                isinplace(f),typeof(p),
