34eac11ed4f29e0fcc86aed696a8cc92cd10cb8a
Christopher Rackauckas <accounts@chrisrackauckas.com>
2020-12-21 11:16:13 -0500

    Update test/ode_default_unstable_check.jl
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>

--- test/ode_default_unstable_check.jl
+++ test/ode_default_unstable_check.jl
@@ -2,38 +2,38 @@ using Test, RecursiveArrayTools, StaticArrays
 
 using DiffEqBase: NAN_CHECK
 
-@test NAN_CHECK(3.0+4.0im) == false
-@test NAN_CHECK(NaN) == true
+@test !NAN_CHECK(3.0+4.0im)
+@test NAN_CHECK(NaN)
 
 u1 = ones(3)
-@test NAN_CHECK(u1) == false
+@test !NAN_CHECK(u1)
 u1′ = copy(u1)
 u1′[2] = NaN
-@test NAN_CHECK(u1′) == true
+@test NAN_CHECK(u1′)
 
 
 u2 = [SA[1.0 1.0; 1.0 1.0] for i = 1:3]
-@test NAN_CHECK(u2) == false
+@test !NAN_CHECK(u2)
 u2′ = copy(u2)
 u2′[2] = SA[1.0 NaN; 1.0 1.0]
-@test NAN_CHECK(u2′) == true
+@test NAN_CHECK(u2′)
 
 u3 = VectorOfArray([ones(5), ones(5)])
-@test NAN_CHECK(u3) == false
+@test !NAN_CHECK(u3)
 u3′ = recursivecopy(u3)
 u3′[2][3] = NaN
-@test NAN_CHECK(u3′) == true
+@test NAN_CHECK(u3′)
 
 u4 = ArrayPartition(u1, u2, u3)
-@test NAN_CHECK(u4) == false
+@test !NAN_CHECK(u4)
 u4_1 = ArrayPartition(u1′, u2, u3)
-@test NAN_CHECK(u4_1) == true
+@test NAN_CHECK(u4_1)
 u4_2 = ArrayPartition(u1, u2′, u3)
-@test NAN_CHECK(u4_2) == true
+@test NAN_CHECK(u4_2)
 u4_3 = ArrayPartition(u1, u2, u3′)
-@test NAN_CHECK(u4_3) == true
+@test NAN_CHECK(u4_3)
 
-@test NAN_CHECK(ArrayPartition(u4, u4)) == false
-@test NAN_CHECK(ArrayPartition(u4, u4_1)) == true
-@test NAN_CHECK(ArrayPartition(u4, u4_2)) == true
-@test NAN_CHECK(ArrayPartition(u4, u4_3)) == true
+@test !NAN_CHECK(ArrayPartition(u4, u4))
+@test NAN_CHECK(ArrayPartition(u4, u4_1))
+@test NAN_CHECK(ArrayPartition(u4, u4_2))
+@test NAN_CHECK(ArrayPartition(u4, u4_3))
