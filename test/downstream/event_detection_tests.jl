42e556e1bcf44282c4df74c55d5444f9ed567fca
Chris Rackauckas <accounts@chrisrackauckas.com>
2021-06-17 19:42:12 -0400

    Use a nudge factor to remove repeats
    Sometimes simple is better. Instead of using a derivative estimate, now we just check at t + dt * repeat_nudge to avoid repeated zeros, with a default to 1//100 which seems to be a nice balance between numerical stability and keeping most of the interval still an eventful zone. Fixes https://github.com/SciML/DifferentialEquations.jl/issues/758

--- test/downstream/event_detection_tests.jl
+++ test/downstream/event_detection_tests.jl
@@ -1,5 +1,6 @@
 using StaticArrays
 using OrdinaryDiffEq
+using LinearAlgebra
 using Test
 
 @inbounds @inline function ż(z, p, t)
@@ -104,7 +105,7 @@ f! = function (du,u,p,t)
   du[2] = -p
 end
 
-function condition!(out,u,t,integrator) 
+function condition!(out,u,t,integrator)
   out[1] = u[1]
   out[2] = u[2]
 end
@@ -127,3 +128,85 @@ begin
     sol = solve(prob,Tsit5(), callback=Vcb)
 end
 
+
+"""
+    eoms(u, p, t)
+
+Equations of motion for the system (6-Dimensional).
+"""
+function eoms(u, p, t)
+    mu = p
+    omm = 1 - mu
+
+    r13 = sqrt((u[1] + mu)^2 + u[2]^2 + u[3]^2)
+    r13_3 = r13^3
+    r23 = sqrt((u[1] - 1 + mu)^2 + u[2]^2 + u[3]^2)
+    r23_3 = r23^3
+
+    Ωx = u[1] - (omm) * (u[1] + mu) / r13_3 - mu * (u[1] - omm) / r23_3
+    Ωy = u[2] - (omm) * u[2] / r13_3 - mu * u[2] / r23_3
+    Ωz = -(omm) * u[3] / r13_3 - mu * u[3] / r23_3
+
+    return @SVector [
+        u[4],
+        u[5],
+        u[6],
+        2u[5] + Ωx,
+        -2u[4] + Ωy,
+        Ωz
+    ]
+end
+
+"""
+    distance(sol, q, t)
+
+Calculate the isochronous position variation between `q` and `sol(t)`.
+"""
+function distance(sol, q, t)
+    posinds = @SVector [1, 2, 3]
+    q_sol = sol(t)
+    return norm(q[posinds] .- q_sol[posinds])
+end
+
+"""
+    distance_callback(sol, threshold)
+
+Create a `ContinuousCallback` that terminates integration when `distance` is
+less than `threshold`.
+"""
+function distance_callback(sol, threshold)
+    condition(u, t, _) = distance(sol, u, t) - threshold
+    affect!(integrator) = nothing
+    affect_neg!(integrator) = terminate!(integrator)
+    return ContinuousCallback(condition, affect!, affect_neg!)
+end
+
+# Mu Parameter
+μ = 0.012150607114626023
+
+# State of Chief at time 0
+q_chief = @SVector [1.0220277279709828, 0.0, -0.18210117430516676, 0.0, -0.1032699633021813, 0.0]
+tof_chief = 10.0
+
+# Integrate the chief trajectory
+prob_chief = ODEProblem{false}(eoms, q_chief, (0.0, tof_chief), μ)
+sol_chief = solve(prob_chief, Vern9(); abstol=1e-12, reltol=1e-12)
+
+# Define point where deputy departs the chief
+t_dep_start = 0.012113772215335508 # Time the deputy trajectory starts at
+
+# Get state of chief at this departure point
+q_depart = sol_chief(t_dep_start)
+
+# Apply change in velocity at that point
+dv = @SVector [0.0, 0.0, 0.0, 0.00017947635829913735, -5.932764319639683e-5, -0.0009575628577891083]
+q_deputy = q_depart + dv
+
+# Build the callback
+threshold = 0.00026014568158168577
+cb = distance_callback(sol_chief, threshold)
+
+# Build & Solve the deputy problem
+tspan_deputy = (t_dep_start, tof_chief - t_dep_start)
+prob_deputy = ODEProblem{false}(eoms, q_deputy, tspan_deputy, μ)
+sol_deputy = solve(prob_deputy, Vern9(); callback=cb, abstol=1e-12, reltol=1e-12)
