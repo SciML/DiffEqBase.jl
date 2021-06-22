501f36b48f5d0cb604e39614f91d0cd7d6ecec3f
Chris Rackauckas <accounts@chrisrackauckas.com>
2021-06-22 04:20:47 -0400

    add some adjoints back

--- src/chainrules.jl
+++ src/chainrules.jl
@@ -1,8 +1,18 @@
-function ChainRulesCore.rrule(::ODESolution{T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11
-                },u,args...) where {T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11}
+function ChainRulesCore.rrule(::Type{<:ODESolution{T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11
+                }},u,args...) where {T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11}
 
                 function ODESolutionAdjoint(ȳ)
                   (NoTangent(),ȳ,ntuple(_->NoTangent(), length(args))...)
+                end
+
+                ODESolution{T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11}(u,args...),ODESolutionAdjoint
+end
+
+ZygoteRules.@adjoint function ODESolution{T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11
+                }(u,args...) where {T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11}
+
+                function ODESolutionAdjoint(ȳ)
+                  (ȳ,ntuple(_->nothing, length(args))...)
                 end
 
                 ODESolution{T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11}(u,args...),ODESolutionAdjoint
@@ -26,11 +36,11 @@ ZygoteRules.@adjoint function ZygoteRules.literal_getproperty(sol::AbstractNoTim
   sol.u,solu_adjoint
 end
 
-function ChainRulesCore.rrule(::typeof(SciMLBase.tmap), f, args::Union{AbstractArray,Tuple}...)
+ZygoteRules.@adjoint function SciMLBase.tmap(f, args::Union{AbstractArray,Tuple}...)
   ∇tmap(__context__, f, args...)
 end
 
-function ChainRulesCore.rrule(::typeof(SciMLBase.responsible_map), f, args::Union{AbstractArray,Tuple}...)
+ZygoteRules.@adjoint function SciMLBase.responsible_map(f, args::Union{AbstractArray,Tuple}...)
   ∇responsible_map(__context__, f, args...)
 end
 
@@ -45,6 +55,19 @@ function ChainRulesCore.rrule(::DiffEqBase.EnsembleSolution,sim,time,converged)
   end
   out,EnsembleSolution_adjoint
 end
+
+ZygoteRules.@adjoint function DiffEqBase.EnsembleSolution(sim,time,converged)
+  out = EnsembleSolution(sim,time,converged)
+  function EnsembleSolution_adjoint(p̄::AbstractArray{T,N}) where {T,N}
+    arrarr = [[p̄[ntuple(x->Colon(),Val(N-2))...,j,i] for j in 1:size(p̄)[end-1]] for i in 1:size(p̄)[end]]
+    (EnsembleSolution(arrarr, 0.0, true),nothing,nothing)
+  end
+  function EnsembleSolution_adjoint(p̄::EnsembleSolution)
+    (p̄,nothing,nothing)
+  end
+  out,EnsembleSolution_adjoint
+end
+
 ZygoteRules.@adjoint ZygoteRules.literal_getproperty(sim::EnsembleSolution, ::Val{:u}) = sim.u, p̄ -> (EnsembleSolution(p̄, 0.0, true),)
 
 #=
