1f63ff408eaebd29e80925322e1a56b2284b395c
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2021-08-06 10:20:04 -0400

    Update src/calculate_residuals.jl
    Co-authored-by: Yingbo Ma <mayingbo5@gmail.com>

--- src/calculate_residuals.jl
+++ src/calculate_residuals.jl
@@ -93,7 +93,7 @@ end
 
 @inline function calculate_residuals!(out::Array, ũ::Array, u₀::Array, u₁::Array, α::Number, ρ::Number, internalnorm::F, t) where F
   @inbounds for i in eachindex(out, ũ, u₀, u₁)
-    out[i] = calculate_residuals(ũ[i], u₀[i], u₁[i], α, ρ, internalnorm,t)
+    out[i] = calculate_residuals(ũ[i], u₀[i], u₁[i], α, ρ, internalnorm, t)
   end
   nothing
 end
