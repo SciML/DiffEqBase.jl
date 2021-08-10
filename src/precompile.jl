7d52f0eb092cba6f88efbf9caee943574f67232a
Chris Rackauckas <accounts@chrisrackauckas.com>
2021-08-10 07:58:49 -0400

    fix precompile

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,9 +1,8 @@
 let
     while true
         _testf(du,u,p,t) = copyto!(du,u)
-        _testf(du,u,p,t) = du .= u
         b = rand(1); x = rand(1)
-        _linsolve(Val{:init},ODEFunction(_testf),b)
+        _linsolve = DEFAULT_LINSOLVE(Val{:init},ODEFunction(_testf),b)
         A = rand(1,1)
         _linsolve(x,A,b,true)
         _linsolve(x,A,b,false)
