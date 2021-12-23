24cde9c57170c0b8d93c99f00d5c100d46f636a5
Christopher Rackauckas <accounts@chrisrackauckas.com>
2021-12-23 09:59:35 -0500

    Remove mapreduce_empty (#723)
    * Remove mapreduce_empty
    
    Suggestion from https://github.com/SciML/DiffEqBase.jl/issues/664 . Requires v1.6 and we're in the future.
    
    * handle cases like a boss
    
    * safer init

--- src/common_defaults.jl
+++ src/common_defaults.jl
@@ -1,7 +1,8 @@
+abs2_and_sum(x,y) = reduce(Base.add_sum,x,init=zero(real(value(eltype(x))))) +
+                    reduce(Base.add_sum,y,init=zero(real(value(eltype(y)))))
 @inline UNITLESS_ABS2(x::Number) = abs2(x)
-@inline UNITLESS_ABS2(x::AbstractArray) = sum(UNITLESS_ABS2, x)
-@inline UNITLESS_ABS2(x::RecursiveArrayTools.ArrayPartition) = sum(UNITLESS_ABS2, x.x)
-Base.mapreduce_empty(::typeof(UNITLESS_ABS2), op, T) = abs2(Base.reduce_empty(op, T))
+@inline UNITLESS_ABS2(x::AbstractArray) = mapreduce(UNITLESS_ABS2,abs2_and_sum, x, init=zero(real(value(eltype(x)))))
+@inline UNITLESS_ABS2(x::RecursiveArrayTools.ArrayPartition) = mapreduce(UNITLESS_ABS2, abs2_and_sum, x.x, init=zero(real(value(eltype(x)))))
 
 @inline recursive_length(u::AbstractArray{<:Number}) = length(u)
 @inline recursive_length(u::Number) = length(u)
