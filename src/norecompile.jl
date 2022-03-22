a88c6a7911a28a39128ec508c01863c1059e7db0
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-03-22 06:08:34 -0400

    Setup FunctionWrappersWrappers norecompile mode
    Needs:
    
    - https://github.com/SciML/SciMLBase.jl/pull/143
    - https://github.com/SciML/OrdinaryDiffEq.jl/pull/1627
    
    ```julia
    using OrdinaryDiffEq
    function f(du, u, p, t)
        du[1] = 0.2u[1]
        du[2] = 0.4u[2]
    end
    u0 = ones(2)
    tspan = (0.0, 1.0)
    prob = ODEProblem{true,false}(f, u0, tspan, Float64[])
    
    function lorenz(du, u, p, t)
        du[1] = 10.0(u[2] - u[1])
        du[2] = u[1] * (28.0 - u[3]) - u[2]
        du[3] = u[1] * u[2] - (8 / 3) * u[3]
    end
    lorenzprob = ODEProblem{true,false}(lorenz, [1.0; 0.0; 0.0], (0.0, 1.0), Float64[])
    typeof(prob) === typeof(lorenzprob) # true
    
    @time sol = solve(prob, Rosenbrock23(autodiff=false))
    @time sol = solve(prob, Rosenbrock23(chunk_size=1))
    ```
    
    ```
    2.763588 seconds (10.32 M allocations: 648.718 MiB, 4.92% gc time, 99.89% compilation time)
    
    10.577789 seconds (45.44 M allocations: 2.760 GiB, 4.87% gc time, 99.97% compilation time)
    ```
    
    While the types of `prob` are exactly the same, there is still a significant amount of compile time, even with that exact same time being called in `using` at OrdinaryDiffEq.
    
    Maybe this needs to be run on master?

---
+++ src/norecompile.jl
@@ -0,0 +1,56 @@
+struct OrdinaryDiffEqTag end
+
+const dualT = ForwardDiff.Dual{ForwardDiff.Tag{OrdinaryDiffEqTag,Float64},Float64,1}
+const arglists = (Tuple{Vector{Float64},Vector{Float64},Vector{Float64},Float64},
+                  Tuple{Vector{Float64},Vector{Float64},SciMLBase.NullParameters,Float64},
+                  Tuple{Vector{dualT},Vector{Float64},Vector{Float64},dualT},
+                  Tuple{Vector{dualT},Vector{dualT},Vector{Float64},Float64},
+                  Tuple{Vector{dualT},Vector{dualT},SciMLBase.NullParameters,Float64},
+                  Tuple{Vector{dualT},Vector{Float64},SciMLBase.NullParameters,dualT})
+const returnlists = ntuple(x -> Nothing, length(arglists))
+void(f) = function (du, u, p, t)
+  f(du, u, p, t)
+  nothing
+end
+const NORECOMPILE_FUNCTION = typeof(FunctionWrappersWrappers.FunctionWrappersWrapper(void(() -> nothing), arglists, returnlists))
+wrap_norecompile(f) = FunctionWrappersWrappers.FunctionWrappersWrapper(void(f), arglists, returnlists)
+
+function ODEFunction{iip,false}(f;
+  mass_matrix=I,
+  analytic=nothing,
+  tgrad=nothing,
+  jac=nothing,
+  jvp=nothing,
+  vjp=nothing,
+  jac_prototype=nothing,
+  sparsity=jac_prototype,
+  Wfact=nothing,
+  Wfact_t=nothing,
+  paramjac=nothing,
+  syms=nothing,
+  indepsym=nothing,
+  observed=SciMLBase.DEFAULT_OBSERVED,
+  colorvec=nothing) where {iip}
+
+  if jac === nothing && isa(jac_prototype, AbstractDiffEqLinearOperator)
+    if iip
+      jac = update_coefficients! #(J,u,p,t)
+    else
+      jac = (u, p, t) -> update_coefficients!(deepcopy(jac_prototype), u, p, t)
+    end
+  end
+
+  if jac_prototype !== nothing && colorvec === nothing && ArrayInterface.fast_matrix_colors(jac_prototype)
+    _colorvec = ArrayInterface.matrix_colors(jac_prototype)
+  else
+    _colorvec = colorvec
+  end
+
+  ODEFunction{iip,
+    NORECOMPILE_FUNCTION,Any,Any,Any,Any,
+    Any,Any,Any,Any,Any,
+    Any,Any,typeof(syms),typeof(indepsym),Any,typeof(_colorvec)}(
+    wrap_norecompile(f), mass_matrix, analytic, tgrad, jac,
+    jvp, vjp, jac_prototype, sparsity, Wfact,
+    Wfact_t, paramjac, syms, indepsym, observed, _colorvec)
+end
