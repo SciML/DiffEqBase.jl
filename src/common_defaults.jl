3bc1ac3a91ee189032ff555527845f1bd272b5e0
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-03-26 11:22:30 -0400

    opposite

--- src/common_defaults.jl
+++ src/common_defaults.jl
@@ -1,7 +1,7 @@
-abs2_and_sum(x,y) = reduce(Base.add_sum,x,init=zero(real(value(eltype(x))))) +
-                    reduce(Base.add_sum,y,init=zero(real(value(eltype(y)))))
+abs2_and_sum(x, y) = reduce(Base.add_sum, x, init=zero(real(value(eltype(x))))) +
+                     reduce(Base.add_sum, y, init=zero(real(value(eltype(y)))))
 @inline UNITLESS_ABS2(x::Number) = abs2(x)
-@inline UNITLESS_ABS2(x::AbstractArray) = mapreduce(UNITLESS_ABS2,abs2_and_sum, x, init=zero(real(value(eltype(x)))))
+@inline UNITLESS_ABS2(x::AbstractArray) = mapreduce(UNITLESS_ABS2, abs2_and_sum, x, init=zero(real(value(eltype(x)))))
 @inline UNITLESS_ABS2(x::RecursiveArrayTools.ArrayPartition) = mapreduce(UNITLESS_ABS2, abs2_and_sum, x.x, init=zero(real(value(eltype(x)))))
 
 @inline recursive_length(u::AbstractArray{<:Number}) = length(u)
@@ -10,9 +10,9 @@ abs2_and_sum(x,y) = reduce(Base.add_sum,x,init=zero(real(value(eltype(x))))) +
 @inline recursive_length(u::RecursiveArrayTools.ArrayPartition) = sum(recursive_length, u.x)
 @inline recursive_length(u::RecursiveArrayTools.VectorOfArray) = sum(recursive_length, u.u)
 
-@inline ODE_DEFAULT_NORM(u::Union{AbstractFloat,Complex},t) = @fastmath abs(u)
+@inline ODE_DEFAULT_NORM(u::Union{AbstractFloat,Complex}, t) = @fastmath abs(u)
 
-@inline function ODE_DEFAULT_NORM(u::Array{T},t) where T<:Union{AbstractFloat,Complex}
+@inline function ODE_DEFAULT_NORM(u::Array{T}, t) where {T<:Union{AbstractFloat,Complex}}
     x = abs2(u[1])
     @inbounds for i in 2:length(u)
         x += abs2(u[i])
@@ -20,23 +20,25 @@ abs2_and_sum(x,y) = reduce(Base.add_sum,x,init=zero(real(value(eltype(x))))) +
     sqrt(real(x) / length(u))
 end
 
-@inline ODE_DEFAULT_NORM(u::StaticArrays.StaticArray{T},t) where T<:Union{AbstractFloat,Complex} =
-                                            sqrt(real(sum(abs2,u)) / length(u))
+@inline ODE_DEFAULT_NORM(u::StaticArrays.StaticArray{T}, t) where {T<:Union{AbstractFloat,Complex}} =
+    sqrt(real(sum(abs2, u)) / length(u))
 
-@inline ODE_DEFAULT_NORM(u::AbstractArray,t) = sqrt(UNITLESS_ABS2(u)/recursive_length(u))
-@inline ODE_DEFAULT_NORM(u,t) = norm(u)
+@inline ODE_DEFAULT_NORM(u::AbstractArray, t) = sqrt(UNITLESS_ABS2(u) / recursive_length(u))
+@inline ODE_DEFAULT_NORM(u, t) = norm(u)
 
-@inline ODE_DEFAULT_ISOUTOFDOMAIN(u,p,t) = false
-@inline function ODE_DEFAULT_PROG_MESSAGE(dt,u::Array,p,t)
+@inline ODE_DEFAULT_ISOUTOFDOMAIN(u, p, t) = false
+@inline function ODE_DEFAULT_PROG_MESSAGE(dt, u::Array, p, t)
     tmp = u[1]
     for i in eachindex(u)
-        tmp = ifelse(abs(u[i]) > abs(tmp),u[i],tmp)
+        tmp = ifelse(abs(u[i]) > abs(tmp), u[i], tmp)
     end
-    "dt="*string(dt)*"\nt="*string(t)*"\nmax u="*string(tmp)
+    "dt=" * string(dt) * "\nt=" * string(t) * "\nmax u=" * string(tmp)
 end
-@inline ODE_DEFAULT_PROG_MESSAGE(dt,u,p,t) =
-           "dt="*string(dt)*"\nt="*string(t)*"\nmax u="*string(maximum(abs.(u)))
+@inline ODE_DEFAULT_PROG_MESSAGE(dt, u, p, t) =
+    "dt=" * string(dt) * "\nt=" * string(t) * "\nmax u=" * string(maximum(abs.(u)))
 
-@inline NAN_CHECK(x::AbstractArray) = isfinite(sum(x))
-@inline ODE_DEFAULT_UNSTABLE_CHECK(dt,u,p,t) = false
-@inline ODE_DEFAULT_UNSTABLE_CHECK(dt,u::Union{Number,AbstractArray},p,t) = NAN_CHECK(u)
+
+@inline NAN_CHECK(x::Number) = !isfinite(x)
+@inline NAN_CHECK(x::AbstractArray) = !isfinite(sum(x))
+@inline ODE_DEFAULT_UNSTABLE_CHECK(dt, u, p, t) = false
+@inline ODE_DEFAULT_UNSTABLE_CHECK(dt, u::Union{Number,AbstractArray}, p, t) = NAN_CHECK(u)
