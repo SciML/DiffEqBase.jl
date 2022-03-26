cabb6d5fdbc9cb194bb1a02cf825dca7698c8714
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-03-26 11:05:20 -0400

    simplify and speedup NaNCheck
    From https://discourse.julialang.org/t/fastest-way-to-check-for-inf-or-nan-in-an-array/76954/22 @oscarsmith

--- src/common_defaults.jl
+++ src/common_defaults.jl
@@ -37,11 +37,6 @@ end
 @inline ODE_DEFAULT_PROG_MESSAGE(dt,u,p,t) =
            "dt="*string(dt)*"\nt="*string(t)*"\nmax u="*string(maximum(abs.(u)))
 
-@inline NAN_CHECK(x::Number) = isnan(x)
-@inline NAN_CHECK(x::Float64) = isnan(x) || (x>1e50)
-@inline NAN_CHECK(x::Enum) = false
-@inline NAN_CHECK(x::AbstractArray) = any(NAN_CHECK, x)
-@inline NAN_CHECK(x::RecursiveArrayTools.ArrayPartition) = any(NAN_CHECK, x.x)
-
+@inline NAN_CHECK(x::AbstractArray) = isfinite(sum(x))
 @inline ODE_DEFAULT_UNSTABLE_CHECK(dt,u,p,t) = false
 @inline ODE_DEFAULT_UNSTABLE_CHECK(dt,u::Union{Number,AbstractArray},p,t) = NAN_CHECK(u)
