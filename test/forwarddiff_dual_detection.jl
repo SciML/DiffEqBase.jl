d211d72b10347338e3d6b8790186f6dfb959ee02
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-06-18 22:12:17 +0200

    split inference testing for v1.7

--- test/forwarddiff_dual_detection.jl
+++ test/forwarddiff_dual_detection.jl
@@ -16,19 +16,34 @@ p_possibilities = [ForwardDiff.Dual(2.0),(ForwardDiff.Dual(2.0),2.0),
     [ForwardDiff.Dual(2.0)],([ForwardDiff.Dual(2.0)],2.0),
     (2.0,ForwardDiff.Dual(2.0)),(;x=2.0,y=ForwardDiff.Dual(2.0)),
     (;x=2.0,y=[ForwardDiff.Dual(2.0)]),(;x=2.0,y=[[ForwardDiff.Dual(2.0)]]),
-    MyStruct(2.0,ForwardDiff.Dual(2.0)),[MyStruct(2.0,ForwardDiff.Dual(2.0))],
-    [MyStruct(2.0,(2.0,ForwardDiff.Dual(2.0)))],[MyStruct(2.0,[2.0,ForwardDiff.Dual(2.0)])],
     Set([2.0,ForwardDiff.Dual(2.0)]),(SciMLBase.NullParameters(),ForwardDiff.Dual(2.0))
 ]
 
 for p in p_possibilities
-    @show p
     @test DiffEqBase.anyeltypedual(p) <: ForwardDiff.Dual
     u0 = 2.0
     @test DiffEqBase.promote_u0(u0,p,t0) isa ForwardDiff.Dual
     u0 = ForwardDiff.Dual(2.0)
     @test DiffEqBase.promote_u0(u0,p,t0) isa ForwardDiff.Dual
     @inferred DiffEqBase.anyeltypedual(p)
+end
+
+p_possibilities17 = [
+    MyStruct(2.0,ForwardDiff.Dual(2.0)),[MyStruct(2.0,ForwardDiff.Dual(2.0))],
+    [MyStruct(2.0,(2.0,ForwardDiff.Dual(2.0)))],[MyStruct(2.0,[2.0,ForwardDiff.Dual(2.0)])],
+]
+
+for p in p_possibilities17
+    @test DiffEqBase.anyeltypedual(p) <: ForwardDiff.Dual
+    u0 = 2.0
+    @test DiffEqBase.promote_u0(u0,p,t0) isa ForwardDiff.Dual
+    u0 = ForwardDiff.Dual(2.0)
+    @test DiffEqBase.promote_u0(u0,p,t0) isa ForwardDiff.Dual
+
+    if VERSION >= v"1.7"
+        # v1.6 does not infer `getproperty` mapping
+        @inferred DiffEqBase.anyeltypedual(p)
+    end
 end
 
 p_possibilities_uninferrred = [
