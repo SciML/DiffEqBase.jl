f378f840f795578ce8293650ebf9fd8e18630a0c
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-06-19 06:25:55 -0400

    Document anyeltypedual, handle empty tuple case and higher order AD
    Fixes https://github.com/SciML/DiffEqBase.jl/issues/774

--- src/forwarddiff.jl
+++ src/forwarddiff.jl
@@ -1,9 +1,49 @@
+"""
+promote_dual(::Type{T},::Type{T2})
+
+Is like the number promotion system, but always prefers a dual number type above
+anything else. For higher order differentiation, it returns the most dualiest of
+them all. This is then used to promote `u0` into the suspected highest differentiation
+space for solving the equation.
+"""
 promote_dual(::Type{T},::Type{T2}) where {T,T2} = T
 promote_dual(::Type{T},::Type{T2}) where {T<:ForwardDiff.Dual,T2} = T
 promote_dual(::Type{T},::Type{T2}) where {T<:ForwardDiff.Dual,T2<:ForwardDiff.Dual} = T
 promote_dual(::Type{T},::Type{T2}) where {T,T2<:ForwardDiff.Dual} = T2
 
-# Catch composite types, check all of their fields
+promote_dual(::Type{T},::Type{T2}) where {T3,T4,V,V2<:ForwardDiff.Dual,N,N2,
+             T<:ForwardDiff.Dual{T3,V,N},
+             T2<:ForwardDiff.Dual{T4,V2,N2}} = T2
+promote_dual(::Type{T},::Type{T2}) where {T3,T4,V<:ForwardDiff.Dual,V2,N,N2,
+             T<:ForwardDiff.Dual{T3,V,N},
+             T2<:ForwardDiff.Dual{T4,V2,N2}} = T
+promote_dual(::Type{T},::Type{T2}) where {
+             T3,V<:ForwardDiff.Dual,V2<:ForwardDiff.Dual,N,
+             T<:ForwardDiff.Dual{T3,V,N},
+             T2<:ForwardDiff.Dual{T3,V2,N}} = ForwardDiff.Dual{T3,promote_dual(V,V2),N}
+
+# Untyped dispatch: catch composite types, check all of their fields
+
+"""
+anyeltypedual(x)
+
+Searches through a type to see if any of its values are parameters. This is used to
+then promote other values to match the dual type. For example, if a user pass a parameter
+which is a `Dual` and a `u0` which is a `Float64`, after the first time step, `f(u,p,t) = p*u`
+will change `u0` from `Float64` to `Dual`. Thus the state variable always needs to be converted
+to a dual number before the solve. Worse still, this needs to be done in the case of 
+`f(du,u,p,t) = du[1] = p*u[1]`, and thus running `f` and taking the return value is not a valid
+way to calculate the required state type.
+
+But given the properties of automatic differentiation requiring that differntiation of parameters
+implies differentiation of state, we assume any dual parameters implies differentiation of state
+and then attempt to upconvert `u0` to match that dual-ness. Because this changes types, this needs
+to be specified at compiled time and thus cannot have a Bool-based opt out, so in the future this
+may be extended to use a preference system to opt-out with a `UPCONVERT_DUALS`. In the case where
+upconversion is not done automatically, the user is required to upconvert all initial conditions
+themselves, for an example of how this can be confusing to a user see 
+https://discourse.julialang.org/t/typeerror-in-julia-turing-when-sampling-for-a-forced-differential-equation/82937
+"""
 function anyeltypedual(x) 
   if propertynames(x) === ()
     Any
@@ -12,11 +52,21 @@ function anyeltypedual(x)
   end
 end
 anyeltypedual(::Type{T}) where T = T
+
+# Any in this context just means not Dual
 anyeltypedual(x::SciMLBase.NullParameters) = Any
 anyeltypedual(x::Number) = typeof(x)
 anyeltypedual(x::Union{AbstractArray{T},Set{T}}) where T<:Number = T
 anyeltypedual(x::Union{AbstractArray,Set}) = mapreduce(anyeltypedual,promote_dual,x)
-anyeltypedual(x::Tuple) = mapreduce(anyeltypedual,promote_dual,x)
+
+function anyeltypedual(x::Tuple)
+  # Handle the empty tuple case separately for inference and to avoid mapreduce error
+  if x === ()
+    Any
+  else
+    mapreduce(anyeltypedual,promote_dual,x)
+  end
+end
 anyeltypedual(x::Union{Dict,NamedTuple}) = mapreduce(anyeltypedual,promote_dual,values(x))
 
 function promote_u0(u0,p,t0) 
--- test/forwarddiff_dual_detection.jl
+++ test/forwarddiff_dual_detection.jl
@@ -16,11 +16,29 @@ p_possibilities = [ForwardDiff.Dual(2.0),(ForwardDiff.Dual(2.0),2.0),
     [ForwardDiff.Dual(2.0)],([ForwardDiff.Dual(2.0)],2.0),
     (2.0,ForwardDiff.Dual(2.0)),(;x=2.0,y=ForwardDiff.Dual(2.0)),
     (;x=2.0,y=[ForwardDiff.Dual(2.0)]),(;x=2.0,y=[[ForwardDiff.Dual(2.0)]]),
-    Set([2.0,ForwardDiff.Dual(2.0)]),(SciMLBase.NullParameters(),ForwardDiff.Dual(2.0))
+    Set([2.0,ForwardDiff.Dual(2.0)]),(SciMLBase.NullParameters(),ForwardDiff.Dual(2.0)),
+    ((),ForwardDiff.Dual(2.0)),ForwardDiff.Dual{Nothing}(ForwardDiff.Dual{MyStruct}(2.0))
 ]
 
 for p in p_possibilities
     @test DiffEqBase.anyeltypedual(p) <: ForwardDiff.Dual
+    u0 = 2.0
+    @test DiffEqBase.promote_u0(u0,p,t0) isa ForwardDiff.Dual
+    u0 = ForwardDiff.Dual(2.0)
+    @test DiffEqBase.promote_u0(u0,p,t0) isa ForwardDiff.Dual
+    @inferred DiffEqBase.anyeltypedual(p)
+end
+
+higher_order_p_possibilities = [ForwardDiff.Dual{Nothing}(ForwardDiff.Dual{MyStruct}(2.0)),
+    (ForwardDiff.Dual{Nothing}(ForwardDiff.Dual{MyStruct}(2.0)), SciMLBase.NullParameters()),
+    (ForwardDiff.Dual{Nothing}(ForwardDiff.Dual{MyStruct}(2.0)), ForwardDiff.Dual{Nothing}(2.0)),
+    (ForwardDiff.Dual{Nothing}(2.0), ForwardDiff.Dual{Nothing}(ForwardDiff.Dual{MyStruct}(2.0)))
+]
+
+for p in higher_order_p_possibilities
+    @show p
+    @test DiffEqBase.anyeltypedual(p) <: ForwardDiff.Dual
+    @test DiffEqBase.anyeltypedual(p) <: ForwardDiff.Dual{Nothing,ForwardDiff.Dual{MyStruct,Float64,0},0}
     u0 = 2.0
     @test DiffEqBase.promote_u0(u0,p,t0) isa ForwardDiff.Dual
     u0 = ForwardDiff.Dual(2.0)
