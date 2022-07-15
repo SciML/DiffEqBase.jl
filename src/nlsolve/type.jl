edab5b95d6b7dd4d30d78b89bb81bb74fe6e5f91
Yingbo Ma <mayingbo5@gmail.com>
2022-07-15 11:41:11 -0400

    Add more Newton options (#794)

--- src/nlsolve/type.jl
+++ src/nlsolve/type.jl
@@ -65,11 +65,13 @@ struct NLNewton{K, C1, C2} <: AbstractNLSolverAlgorithm
     max_iter::Int
     fast_convergence_cutoff::C1
     new_W_dt_cutoff::C2
+    always_new::Bool
+    check_div::Bool
 end
 
 function NLNewton(; κ = 1 // 100, max_iter = 10, fast_convergence_cutoff = 1 // 5,
-                  new_W_dt_cutoff = 1 // 5)
-    NLNewton(κ, max_iter, fast_convergence_cutoff, new_W_dt_cutoff)
+                  new_W_dt_cutoff = 1 // 5, always_new = false, check_div = true)
+    NLNewton(κ, max_iter, fast_convergence_cutoff, new_W_dt_cutoff, always_new, check_div)
 end
 
 # caches
