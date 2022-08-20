5da8e11da72b2b7c22d607be3592e9117e2f450a
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-08-20 07:40:38 -0400

    do not require SciMLBase changes and support oop

--- src/norecompile.jl
+++ src/norecompile.jl
@@ -1,99 +1,152 @@
 struct OrdinaryDiffEqTag end
 
-const dualT = ForwardDiff.Dual{ForwardDiff.Tag{OrdinaryDiffEqTag,Float64},Float64,1}
-const arglists = (Tuple{Vector{Float64},Vector{Float64},Vector{Float64},Float64},
-                  Tuple{Vector{Float64},Vector{Float64},SciMLBase.NullParameters,Float64},
-                  Tuple{Vector{dualT},Vector{Float64},Vector{Float64},dualT},
-                  Tuple{Vector{dualT},Vector{dualT},Vector{Float64},Float64},
-                  Tuple{Vector{dualT},Vector{dualT},SciMLBase.NullParameters,Float64},
-                  Tuple{Vector{dualT},Vector{Float64},SciMLBase.NullParameters,dualT})
-const returnlists = ntuple(x -> Nothing, length(arglists))
-function void(@nospecialize(f::Function)) 
-  function f2(@nospecialize(du::Vector{Float64}), @nospecialize(u::Vector{Float64}), @nospecialize(p::Vector{Float64}), @nospecialize(t::Float64))
-    f(du, u, p, t)
-    nothing
-  end
+const dualT = ForwardDiff.Dual{ForwardDiff.Tag{OrdinaryDiffEqTag, Float64}, Float64, 1}
+const NORECOMPILE_SUPPORTED_ARGS = (Tuple{Vector{Float64}, Vector{Float64},
+                                          Vector{Float64}, Float64},
+                                    Tuple{Vector{Float64}, Vector{Float64},
+                                          SciMLBase.NullParameters, Float64})
+const arglists = (Tuple{Vector{Float64}, Vector{Float64}, Vector{Float64}, Float64},
+                  Tuple{Vector{Float64}, Vector{Float64}, SciMLBase.NullParameters, Float64
+                        },
+                  Tuple{Vector{dualT}, Vector{Float64}, Vector{Float64}, dualT},
+                  Tuple{Vector{dualT}, Vector{dualT}, Vector{Float64}, Float64},
+                  Tuple{Vector{dualT}, Vector{dualT}, SciMLBase.NullParameters, Float64},
+                  Tuple{Vector{dualT}, Vector{Float64}, SciMLBase.NullParameters, dualT})
+const iip_returnlists = ntuple(x -> Nothing, length(arglists))
+function void(@nospecialize(f::Function))
+    function f2(@nospecialize(du::Vector{Float64}), @nospecialize(u::Vector{Float64}),
+                @nospecialize(p::Vector{Float64}), @nospecialize(t::Float64))
+        f(du, u, p, t)
+        nothing
+    end
 
-  function f2(@nospecialize(du::Vector{Float64}), @nospecialize(u::Vector{Float64}), @nospecialize(p::SciMLBase.NullParameters), @nospecialize(t::Float64))
-    f(du, u, p, t)
-    nothing
-  end
+    function f2(@nospecialize(du::Vector{Float64}), @nospecialize(u::Vector{Float64}),
+                @nospecialize(p::SciMLBase.NullParameters), @nospecialize(t::Float64))
+        f(du, u, p, t)
+        nothing
+    end
 
-  function f2(@nospecialize(du::Vector{dualT}), @nospecialize(u::Vector{dualT}), @nospecialize(p::Vector{Float64}), @nospecialize(t::Float64))
-    f(du, u, p, t)
-    nothing
-  end
+    function f2(@nospecialize(du::Vector{dualT}), @nospecialize(u::Vector{dualT}),
+                @nospecialize(p::Vector{Float64}), @nospecialize(t::Float64))
+        f(du, u, p, t)
+        nothing
+    end
 
-  function f2(@nospecialize(du::Vector{dualT}), @nospecialize(u::Vector{dualT}), @nospecialize(p::SciMLBase.NullParameters), @nospecialize(t::Float64))
-    f(du, u, p, t)
-    nothing
-  end
-  
-  function f2(@nospecialize(du::Vector{dualT}), @nospecialize(u::Vector{Float64}), @nospecialize(p::Vector{Float64}), @nospecialize(t::dualT))
-    f(du, u, p, t)
-    nothing
-  end
+    function f2(@nospecialize(du::Vector{dualT}), @nospecialize(u::Vector{dualT}),
+                @nospecialize(p::SciMLBase.NullParameters), @nospecialize(t::Float64))
+        f(du, u, p, t)
+        nothing
+    end
 
-  function f2(@nospecialize(du::Vector{dualT}), @nospecialize(u::Vector{Float64}), @nospecialize(p::SciMLBase.NullParameters), @nospecialize(t::dualT))
-    f(du, u, p, t)
-    nothing
-  end
-  precompile(f, (Vector{Float64}, Vector{Float64}, Vector{Float64}, Float64))
-  precompile(f, (Vector{Float64}, Vector{Float64}, SciMLBase.NullParameters, Float64))
-  precompile(f, (Vector{dualT}, Vector{dualT}, Vector{Float64}, Float64))
-  precompile(f, (Vector{dualT}, Vector{dualT}, SciMLBase.NullParameters, Float64))
-  precompile(f, (Vector{dualT}, Vector{Float64}, Vector{Float64}, dualT))
-  precompile(f, (Vector{dualT}, Vector{Float64}, SciMLBase.NullParameters, dualT))
+    function f2(@nospecialize(du::Vector{dualT}), @nospecialize(u::Vector{Float64}),
+                @nospecialize(p::Vector{Float64}), @nospecialize(t::dualT))
+        f(du, u, p, t)
+        nothing
+    end
 
-  precompile(f2, (Vector{Float64}, Vector{Float64}, Vector{Float64}, Float64))
-  precompile(f2, (Vector{Float64}, Vector{Float64}, SciMLBase.NullParameters, Float64))
-  precompile(f2, (Vector{dualT}, Vector{dualT}, Vector{Float64}, Float64))
-  precompile(f2, (Vector{dualT}, Vector{dualT}, SciMLBase.NullParameters, Float64))
-  precompile(f2, (Vector{dualT}, Vector{Float64}, Vector{Float64}, dualT))
-  precompile(f2, (Vector{dualT}, Vector{Float64}, SciMLBase.NullParameters, dualT))
-  f2
+    function f2(@nospecialize(du::Vector{dualT}), @nospecialize(u::Vector{Float64}),
+                @nospecialize(p::SciMLBase.NullParameters), @nospecialize(t::dualT))
+        f(du, u, p, t)
+        nothing
+    end
+    precompile(f, (Vector{Float64}, Vector{Float64}, Vector{Float64}, Float64))
+    precompile(f, (Vector{Float64}, Vector{Float64}, SciMLBase.NullParameters, Float64))
+    precompile(f, (Vector{dualT}, Vector{dualT}, Vector{Float64}, Float64))
+    precompile(f, (Vector{dualT}, Vector{dualT}, SciMLBase.NullParameters, Float64))
+    precompile(f, (Vector{dualT}, Vector{Float64}, Vector{Float64}, dualT))
+    precompile(f, (Vector{dualT}, Vector{Float64}, SciMLBase.NullParameters, dualT))
+
+    precompile(f2, (Vector{Float64}, Vector{Float64}, Vector{Float64}, Float64))
+    precompile(f2, (Vector{Float64}, Vector{Float64}, SciMLBase.NullParameters, Float64))
+    precompile(f2, (Vector{dualT}, Vector{dualT}, Vector{Float64}, Float64))
+    precompile(f2, (Vector{dualT}, Vector{dualT}, SciMLBase.NullParameters, Float64))
+    precompile(f2, (Vector{dualT}, Vector{Float64}, Vector{Float64}, dualT))
+    precompile(f2, (Vector{dualT}, Vector{Float64}, SciMLBase.NullParameters, dualT))
+    f2
 end
 
-const NORECOMPILE_FUNCTION = typeof(FunctionWrappersWrappers.FunctionWrappersWrapper(void(() -> nothing), arglists, returnlists))
-wrap_norecompile(f) = FunctionWrappersWrappers.FunctionWrappersWrapper(void(f), arglists, returnlists)
+const oop_returnlists = (Vector{Float64},Vector{Float64},
+                         ntuple(x -> Vector{dualT}, length(arglists)-2)...)
 
-function ODEFunction{iip,false}(f;
-  mass_matrix=I,
-  analytic=nothing,
-  tgrad=nothing,
-  jac=nothing,
-  jvp=nothing,
-  vjp=nothing,
-  jac_prototype=nothing,
-  sparsity=jac_prototype,
-  Wfact=nothing,
-  Wfact_t=nothing,
-  paramjac=nothing,
-  syms=nothing,
-  indepsym=nothing,
-  observed=SciMLBase.DEFAULT_OBSERVED,
-  colorvec=nothing, sys = nothing) where {iip}
-
-  if jac === nothing && isa(jac_prototype, AbstractDiffEqLinearOperator)
-    if iip
-      jac = update_coefficients! #(J,u,p,t)
-    else
-      jac = (u, p, t) -> update_coefficients!(deepcopy(jac_prototype), u, p, t)
+function typestablemapping(@nospecialize(f::Function))
+    function f2(@nospecialize(du::Vector{Float64}), @nospecialize(u::Vector{Float64}),
+                @nospecialize(p::Vector{Float64}), @nospecialize(t::Float64))
+        f(u, p, t)::Vector{Float64}
     end
-  end
 
-  if jac_prototype !== nothing && colorvec === nothing && ArrayInterface.fast_matrix_colors(jac_prototype)
-    _colorvec = ArrayInterface.matrix_colors(jac_prototype)
-  else
-    _colorvec = colorvec
-  end
+    function f2(@nospecialize(du::Vector{Float64}), @nospecialize(u::Vector{Float64}),
+                @nospecialize(p::SciMLBase.NullParameters), @nospecialize(t::Float64))
+        f(u, p, t)::Vector{Float64}
+    end
 
-  ODEFunction{iip,
-    NORECOMPILE_FUNCTION,typeof(mass_matrix),typeof(analytic),typeof(tgrad),typeof(jac),
-    typeof(jvp),typeof(vjp),typeof(jac_prototype),typeof(sparsity),typeof(Wfact),
-    typeof(Wfact_t),typeof(paramjac),typeof(syms),typeof(indepsym),
-    typeof(observed),typeof(_colorvec), typeof(sys)}(
-    wrap_norecompile(f), mass_matrix, analytic, tgrad, jac,
-    jvp, vjp, jac_prototype, sparsity, Wfact,
-    Wfact_t, paramjac, syms, indepsym, observed, _colorvec, sys)
+    function f2(@nospecialize(du::Vector{dualT}), @nospecialize(u::Vector{dualT}),
+                @nospecialize(p::Vector{Float64}), @nospecialize(t::Float64))
+        f(u, p, t)::Vector{dualT}
+    end
+
+    function f2(@nospecialize(du::Vector{dualT}), @nospecialize(u::Vector{dualT}),
+                @nospecialize(p::SciMLBase.NullParameters), @nospecialize(t::Float64))
+        f(u, p, t)::Vector{dualT}
+    end
+
+    function f2(@nospecialize(du::Vector{dualT}), @nospecialize(u::Vector{Float64}),
+                @nospecialize(p::Vector{Float64}), @nospecialize(t::dualT))
+        f(u, p, t)::Vector{dualT}
+    end
+
+    function f2(@nospecialize(du::Vector{dualT}), @nospecialize(u::Vector{Float64}),
+                @nospecialize(p::SciMLBase.NullParameters), @nospecialize(t::dualT))
+        f(u, p, t)::Vector{dualT}
+    end
+    precompile(f, (Vector{Float64}, Vector{Float64}, Vector{Float64}, Float64))
+    precompile(f, (Vector{Float64}, Vector{Float64}, SciMLBase.NullParameters, Float64))
+    precompile(f, (Vector{dualT}, Vector{dualT}, Vector{Float64}, Float64))
+    precompile(f, (Vector{dualT}, Vector{dualT}, SciMLBase.NullParameters, Float64))
+    precompile(f, (Vector{dualT}, Vector{Float64}, Vector{Float64}, dualT))
+    precompile(f, (Vector{dualT}, Vector{Float64}, SciMLBase.NullParameters, dualT))
+
+    precompile(f2, (Vector{Float64}, Vector{Float64}, Vector{Float64}, Float64))
+    precompile(f2, (Vector{Float64}, Vector{Float64}, SciMLBase.NullParameters, Float64))
+    precompile(f2, (Vector{dualT}, Vector{dualT}, Vector{Float64}, Float64))
+    precompile(f2, (Vector{dualT}, Vector{dualT}, SciMLBase.NullParameters, Float64))
+    precompile(f2, (Vector{dualT}, Vector{Float64}, Vector{Float64}, dualT))
+    precompile(f2, (Vector{dualT}, Vector{Float64}, SciMLBase.NullParameters, dualT))
+    f2
+end
+
+const NORECOMPILE_ARGUMENT_MESSAGE = """
+                                     No-recompile mode is only supported for state arguments
+                                     of type `Vector{Float64}`, time arguments of `Float64`
+                                     and parameter arguments of type `Vector{Float64}` or
+                                     `SciMLBase.NullParameters`.
+                                     """
+
+struct NoRecompileArgumentError <: Exception
+    args
+end
+
+function Base.showerror(io::IO, e::NoRecompileArgumentError)
+    println(io, NORECOMPILE_ARGUMENT_MESSAGE)
+    print(io, "Attempted arguments: ")
+    print(io, e.args)
+end
+
+function wrapfun_oop(ff, inputs::Tuple)
+    IT = Tuple{map(typeof, inputs)...}
+    if IT ∉ NORECOMPILE_SUPPORTED_ARGS
+        throw(NoRecompileArgumentError(IT))
+    end
+    FunctionWrappersWrappers.FunctionWrappersWrapper(void(ff), arglists, oop_returnlists)
+end
+
+function wrapfun_iip(ff, inputs::Tuple)
+    IT = Tuple{map(typeof, inputs)...}
+    if IT ∉ NORECOMPILE_SUPPORTED_ARGS
+        throw(NoRecompileArgumentError(IT))
+    end
+    FunctionWrappersWrappers.FunctionWrappersWrapper(void(ff), arglists, iip_returnlists)
+end
+
+function unwrap_fw(fw::FunctionWrapper)
+    fw.obj[]
 end
--- src/utils.jl
+++ src/utils.jl
@@ -1,17 +1,3 @@
-function wrapfun_oop(ff, inputs::Tuple)
-    IT = map(typeof, inputs)
-    FunctionWrapper{IT[1], Tuple{IT...}}((args...) -> (ff(args...)))
-end
-
-function wrapfun_iip(ff, inputs::Tuple)
-    IT = map(typeof, inputs)
-    FunctionWrapper{Nothing, Tuple{IT...}}((args...) -> (ff(args...); nothing))
-end
-
-function unwrap_fw(fw::FunctionWrapper)
-    fw.obj[]
-end
-
 _vec(v) = vec(v)
 _vec(v::Number) = v
 _vec(v::AbstractVector) = v
