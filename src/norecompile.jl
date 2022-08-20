c91f55d59a6c1aa9fc34fb13a281d636461bf4c4
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-08-20 08:45:40 -0400

    fix oop handling

--- src/norecompile.jl
+++ src/norecompile.jl
@@ -1,18 +1,21 @@
 struct OrdinaryDiffEqTag end
 
 const dualT = ForwardDiff.Dual{ForwardDiff.Tag{OrdinaryDiffEqTag, Float64}, Float64, 1}
-const NORECOMPILE_SUPPORTED_ARGS = (Tuple{Vector{Float64}, Vector{Float64},
-                                          Vector{Float64}, Float64},
-                                    Tuple{Vector{Float64}, Vector{Float64},
-                                          SciMLBase.NullParameters, Float64})
-const arglists = (Tuple{Vector{Float64}, Vector{Float64}, Vector{Float64}, Float64},
-                  Tuple{Vector{Float64}, Vector{Float64}, SciMLBase.NullParameters, Float64
-                        },
-                  Tuple{Vector{dualT}, Vector{Float64}, Vector{Float64}, dualT},
-                  Tuple{Vector{dualT}, Vector{dualT}, Vector{Float64}, Float64},
-                  Tuple{Vector{dualT}, Vector{dualT}, SciMLBase.NullParameters, Float64},
-                  Tuple{Vector{dualT}, Vector{Float64}, SciMLBase.NullParameters, dualT})
-const iip_returnlists = ntuple(x -> Nothing, length(arglists))
+const NORECOMPILE_IIP_SUPPORTED_ARGS = (Tuple{Vector{Float64}, Vector{Float64},
+                                              Vector{Float64}, Float64},
+                                        Tuple{Vector{Float64}, Vector{Float64},
+                                              SciMLBase.NullParameters, Float64})
+const iip_arglists = (Tuple{Vector{Float64}, Vector{Float64}, Vector{Float64}, Float64},
+                      Tuple{Vector{Float64}, Vector{Float64}, SciMLBase.NullParameters,
+                            Float64
+                            },
+                      Tuple{Vector{dualT}, Vector{Float64}, Vector{Float64}, dualT},
+                      Tuple{Vector{dualT}, Vector{dualT}, Vector{Float64}, Float64},
+                      Tuple{Vector{dualT}, Vector{dualT}, SciMLBase.NullParameters, Float64
+                            },
+                      Tuple{Vector{dualT}, Vector{Float64}, SciMLBase.NullParameters, dualT
+                            })
+const iip_returnlists = ntuple(x -> Nothing, length(iip_arglists))
 function void(@nospecialize(f::Function))
     function f2(@nospecialize(du::Vector{Float64}), @nospecialize(u::Vector{Float64}),
                 @nospecialize(p::Vector{Float64}), @nospecialize(t::Float64))
@@ -65,52 +68,63 @@ function void(@nospecialize(f::Function))
     f2
 end
 
-const oop_returnlists = (Vector{Float64},Vector{Float64},
-                         ntuple(x -> Vector{dualT}, length(arglists)-2)...)
+const oop_arglists = (Tuple{Vector{Float64}, Vector{Float64}, Float64},
+                      Tuple{Vector{Float64}, SciMLBase.NullParameters, Float64},
+                      Tuple{Vector{Float64}, Vector{Float64}, dualT},
+                      Tuple{Vector{dualT}, Vector{Float64}, Float64},
+                      Tuple{Vector{dualT}, SciMLBase.NullParameters, Float64},
+                      Tuple{Vector{Float64}, SciMLBase.NullParameters, dualT})
+
+const NORECOMPILE_OOP_SUPPORTED_ARGS = (Tuple{Vector{Float64},
+                                              Vector{Float64}, Float64},
+                                        Tuple{Vector{Float64},
+                                              SciMLBase.NullParameters, Float64})
+const oop_returnlists = (Vector{Float64}, Vector{Float64},
+                         ntuple(x -> Vector{dualT}, length(oop_arglists) - 2)...)
 
 function typestablemapping(@nospecialize(f::Function))
-    function f2(@nospecialize(du::Vector{Float64}), @nospecialize(u::Vector{Float64}),
+    function f2(@nospecialize(u::Vector{Float64}),
                 @nospecialize(p::Vector{Float64}), @nospecialize(t::Float64))
         f(u, p, t)::Vector{Float64}
     end
 
-    function f2(@nospecialize(du::Vector{Float64}), @nospecialize(u::Vector{Float64}),
+    function f2(@nospecialize(u::Vector{Float64}),
                 @nospecialize(p::SciMLBase.NullParameters), @nospecialize(t::Float64))
         f(u, p, t)::Vector{Float64}
     end
 
-    function f2(@nospecialize(du::Vector{dualT}), @nospecialize(u::Vector{dualT}),
+    function f2(@nospecialize(u::Vector{dualT}),
                 @nospecialize(p::Vector{Float64}), @nospecialize(t::Float64))
         f(u, p, t)::Vector{dualT}
     end
 
-    function f2(@nospecialize(du::Vector{dualT}), @nospecialize(u::Vector{dualT}),
+    function f2(@nospecialize(u::Vector{dualT}),
                 @nospecialize(p::SciMLBase.NullParameters), @nospecialize(t::Float64))
         f(u, p, t)::Vector{dualT}
     end
 
-    function f2(@nospecialize(du::Vector{dualT}), @nospecialize(u::Vector{Float64}),
+    function f2(@nospecialize(u::Vector{Float64}),
                 @nospecialize(p::Vector{Float64}), @nospecialize(t::dualT))
         f(u, p, t)::Vector{dualT}
     end
 
-    function f2(@nospecialize(du::Vector{dualT}), @nospecialize(u::Vector{Float64}),
+    function f2(@nospecialize(u::Vector{Float64}),
                 @nospecialize(p::SciMLBase.NullParameters), @nospecialize(t::dualT))
         f(u, p, t)::Vector{dualT}
     end
-    precompile(f, (Vector{Float64}, Vector{Float64}, Vector{Float64}, Float64))
-    precompile(f, (Vector{Float64}, Vector{Float64}, SciMLBase.NullParameters, Float64))
-    precompile(f, (Vector{dualT}, Vector{dualT}, Vector{Float64}, Float64))
-    precompile(f, (Vector{dualT}, Vector{dualT}, SciMLBase.NullParameters, Float64))
-    precompile(f, (Vector{dualT}, Vector{Float64}, Vector{Float64}, dualT))
-    precompile(f, (Vector{dualT}, Vector{Float64}, SciMLBase.NullParameters, dualT))
+    precompile(f, (Vector{Float64}, Vector{Float64}, Float64))
+    precompile(f, (Vector{Float64}, SciMLBase.NullParameters, Float64))
+    precompile(f, (Vector{dualT}, Vector{Float64}, Float64))
+    precompile(f, (Vector{dualT}, SciMLBase.NullParameters, Float64))
+    precompile(f, (Vector{Float64}, Vector{Float64}, dualT))
+    precompile(f, (Vector{Float64}, SciMLBase.NullParameters, dualT))
 
-    precompile(f2, (Vector{Float64}, Vector{Float64}, Vector{Float64}, Float64))
-    precompile(f2, (Vector{Float64}, Vector{Float64}, SciMLBase.NullParameters, Float64))
-    precompile(f2, (Vector{dualT}, Vector{dualT}, Vector{Float64}, Float64))
-    precompile(f2, (Vector{dualT}, Vector{dualT}, SciMLBase.NullParameters, Float64))
-    precompile(f2, (Vector{dualT}, Vector{Float64}, Vector{Float64}, dualT))
-    precompile(f2, (Vector{dualT}, Vector{Float64}, SciMLBase.NullParameters, dualT))
+    precompile(f2, (Vector{Float64}, Vector{Float64}, Float64))
+    precompile(f2, (Vector{Float64}, SciMLBase.NullParameters, Float64))
+    precompile(f2, (Vector{dualT}, Vector{Float64}, Float64))
+    precompile(f2, (Vector{dualT}, SciMLBase.NullParameters, Float64))
+    precompile(f2, (Vector{Float64}, Vector{Float64}, dualT))
+    precompile(f2, (Vector{Float64}, SciMLBase.NullParameters, dualT))
     f2
 end
 
@@ -122,7 +136,7 @@ const NORECOMPILE_ARGUMENT_MESSAGE = """
                                      """
 
 struct NoRecompileArgumentError <: Exception
-    args
+    args::Any
 end
 
 function Base.showerror(io::IO, e::NoRecompileArgumentError)
@@ -133,18 +147,20 @@ end
 
 function wrapfun_oop(ff, inputs::Tuple)
     IT = Tuple{map(typeof, inputs)...}
-    if IT ∉ NORECOMPILE_SUPPORTED_ARGS
+    if IT ∉ NORECOMPILE_OOP_SUPPORTED_ARGS
         throw(NoRecompileArgumentError(IT))
     end
-    FunctionWrappersWrappers.FunctionWrappersWrapper(void(ff), arglists, oop_returnlists)
+    FunctionWrappersWrappers.FunctionWrappersWrapper(typestablemapping(ff), oop_arglists,
+                                                     oop_returnlists)
 end
 
 function wrapfun_iip(ff, inputs::Tuple)
     IT = Tuple{map(typeof, inputs)...}
-    if IT ∉ NORECOMPILE_SUPPORTED_ARGS
+    if IT ∉ NORECOMPILE_IIP_SUPPORTED_ARGS
         throw(NoRecompileArgumentError(IT))
     end
-    FunctionWrappersWrappers.FunctionWrappersWrapper(void(ff), arglists, iip_returnlists)
+    FunctionWrappersWrappers.FunctionWrappersWrapper(void(ff), iip_arglists,
+                                                     iip_returnlists)
 end
 
 function unwrap_fw(fw::FunctionWrapper)
