687e054fd4f5d2ce10c66e96d9f6fd8558f92fa4
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-09-09 19:19:27 -1000

    Opt mass matrices out of autospecialize

--- src/solve.jl
+++ src/solve.jl
@@ -964,6 +964,9 @@ function promote_f(f::F, ::Val{specialize}, u0, p, t) where {F, specialize}
 
     @static if VERSION >= v"1.8-"
         f = if f isa ODEFunction && isinplace(f) && !(f.f isa AbstractDiffEqOperator) &&
+                # Some reinitialization code still uses NLSolvers stuff which doesn't
+                # properly tag, so opt-out if potentially a mass matrix DAE
+               f.mass_matrix === I &&
                ((specialize === SciMLBase.AutoSpecialize && eltype(u0) !== Any &&
                  RecursiveArrayTools.recursive_unitless_eltype(u0) === eltype(u0) &&
                  one(t) === oneunit(t) &&
