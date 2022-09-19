fe8dd65bd9186c9ce946daafdd8690036e0bdd1c
Oscar Smith <oscardssmith@gmail.com>
2022-09-19 08:57:20 -0400

    typo

--- src/utils.jl
+++ src/utils.jl
@@ -20,9 +20,9 @@ function prob2dtmin(tspan, ::Union{AbstractFloat, ForwardDiff.Dual}, use_end_tim
     t1, t2 = tspan
     isfinite(t1) || throw(ArgumentError("t0 in the tspan `(t0, t1)` must be finite"))
     if use_end_time && isfinite(t2 - t1)
-        return 10*eps(t2 - t1)
+        return 10 * eps(t2 - t1)
     else
-        return max(eps(typeof(t1)), 10*eps(t1))
+        return max(eps(typeof(t1)), 10 * eps(t1))
     end
 end
 prob2dtmin(tspan, ::Integer, ::Any) = 0
