97d7890c50063748bd24b1483c0555b928bd8b37
Hendrik Ranocha <ranocha@users.noreply.github.com>
2022-10-05 16:19:32 +0200

    Update forwarddiff.jl

--- src/forwarddiff.jl
+++ src/forwarddiff.jl
@@ -219,7 +219,7 @@ end
 function promote_tspan(u0::AbstractArray{<:ForwardDiff.Dual}, p, tspan, prob, kwargs)
     if (haskey(kwargs, :callback) && has_continuous_callback(kwargs[:callback])) ||
        (haskey(prob.kwargs, :callback) && has_continuous_callback(prob.kwargs[:callback]))
-        return eltype(u0).(tspan)
+        return _promote_tspan(eltype(u0).(tspan), kwargs)
     else
         return _promote_tspan(tspan, kwargs)
     end
