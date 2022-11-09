d46f150e93f54471da92a1ae75545739c9c953df
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-11-09 20:59:43 +0100

    update returncode

--- test/gpu/simple_gpu.jl
+++ test/gpu/simple_gpu.jl
@@ -25,30 +25,30 @@ tspan = (0.0f0, 100.0f0)
 
 prob = ODEProblem(ff, u0, tspan)
 sol = solve(prob, Tsit5())
-@test solve(prob, Rosenbrock23()).retcode == :Success
+@test solve(prob, Rosenbrock23()).retcode == ReturnCode.Success
 solve(prob, Rosenbrock23(autodiff = false));
 
 prob_oop = ODEProblem{false}(ff, u0, tspan)
 CUDA.allowscalar(false)
 sol = solve(prob_oop, Tsit5())
-@test solve(prob_oop, Rosenbrock23()).retcode == :Success
-@test solve(prob_oop, Rosenbrock23(autodiff = false)).retcode == :Success
+@test solve(prob_oop, Rosenbrock23()).retcode == ReturnCode.Success
+@test solve(prob_oop, Rosenbrock23(autodiff = false)).retcode == ReturnCode.Success
 
 prob_nojac = ODEProblem(f, u0, tspan)
-@test solve(prob_nojac, Rosenbrock23()).retcode == :Success
-@test solve(prob_nojac, Rosenbrock23(autodiff = false)).retcode == :Success
+@test solve(prob_nojac, Rosenbrock23()).retcode == ReturnCode.Success
+@test solve(prob_nojac, Rosenbrock23(autodiff = false)).retcode == ReturnCode.Success
 @test solve(prob_nojac,
-            Rosenbrock23(autodiff = false, diff_type = Val{:central})).retcode == :Success
+            Rosenbrock23(autodiff = false, diff_type = Val{:central})).retcode == ReturnCode.Success
 @test solve(prob_nojac,
-            Rosenbrock23(autodiff = false, diff_type = Val{:complex})).retcode == :Success
+            Rosenbrock23(autodiff = false, diff_type = Val{:complex})).retcode == ReturnCode.Success
 
 #=
 prob_nojac_oop = ODEProblem{false}(f,u0,tspan)
 DiffEqBase.prob2dtmin(prob_nojac_oop)
-@test_broken solve(prob_nojac_oop,Rosenbrock23()).retcode == :Success
-@test_broken solve(prob_nojac_oop,Rosenbrock23(autodiff=false)).retcode == :Success
-@test_broken solve(prob_nojac_oop,Rosenbrock23(autodiff=false,diff_type = Val{:central})).retcode == :Success
-@test_broken solve(prob_nojac_oop,Rosenbrock23(autodiff=false,diff_type = Val{:complex})).retcode == :Success
+@test_broken solve(prob_nojac_oop,Rosenbrock23()).retcode == ReturnCode.Success
+@test_broken solve(prob_nojac_oop,Rosenbrock23(autodiff=false)).retcode == ReturnCode.Success
+@test_broken solve(prob_nojac_oop,Rosenbrock23(autodiff=false,diff_type = Val{:central})).retcode == ReturnCode.Success
+@test_broken solve(prob_nojac_oop,Rosenbrock23(autodiff=false,diff_type = Val{:complex})).retcode == ReturnCode.Success
 =#
 
 # Complex Numbers Adaptivity DifferentialEquations.jl#460
