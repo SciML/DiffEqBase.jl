ec1301c805822455f1f32cbcd7873a84ccb3978f
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-11-09 21:14:32 +0100

    format

--- test/gpu/simple_gpu.jl
+++ test/gpu/simple_gpu.jl
@@ -38,9 +38,11 @@ prob_nojac = ODEProblem(f, u0, tspan)
 @test solve(prob_nojac, Rosenbrock23()).retcode == ReturnCode.Success
 @test solve(prob_nojac, Rosenbrock23(autodiff = false)).retcode == ReturnCode.Success
 @test solve(prob_nojac,
-            Rosenbrock23(autodiff = false, diff_type = Val{:central})).retcode == ReturnCode.Success
+            Rosenbrock23(autodiff = false, diff_type = Val{:central})).retcode ==
+      ReturnCode.Success
 @test solve(prob_nojac,
-            Rosenbrock23(autodiff = false, diff_type = Val{:complex})).retcode == ReturnCode.Success
+            Rosenbrock23(autodiff = false, diff_type = Val{:complex})).retcode ==
+      ReturnCode.Success
 
 #=
 prob_nojac_oop = ODEProblem{false}(f,u0,tspan)
