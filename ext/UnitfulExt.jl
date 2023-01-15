009e0f05b3948bb6e95f3769b4a2b4537b03065a
Christopher Rackauckas <accounts@chrisrackauckas.com>
2023-01-15 11:35:47 -0500

    Update UnitfulExt.jl

--- ext/UnitfulExt.jl
+++ ext/UnitfulExt.jl
@@ -1,21 +1,22 @@
 module UnitfulExt
 
 using DiffEqBase
+import DiffEqBase: value
 isdefined(Base, :get_extension) ? (using Unitful) : (using ..Unitful)
 
 # Support adaptive errors should be errorless for exponentiation
-DiffEqBase.value(x::Type{Unitful.AbstractQuantity{T, D, U}}) where {T, D, U} = T
-DiffEqBase.value(x::Unitful.AbstractQuantity) = x.val
+value(x::Type{Unitful.AbstractQuantity{T, D, U}}) where {T, D, U} = T
+value(x::Unitful.AbstractQuantity) = x.val
 @inline function DiffEqBase.ODE_DEFAULT_NORM(u::AbstractArray{<:Unitful.AbstractQuantity, N
                                                               },
                                              t) where {N}
     sqrt(sum(x -> DiffEqBase.ODE_DEFAULT_NORM(x[1], x[2]),
-             zip((DiffEqBase.value(x) for x in u), Iterators.repeated(t))) / length(u))
+             zip((value(x) for x in u), Iterators.repeated(t))) / length(u))
 end
 @inline function DiffEqBase.ODE_DEFAULT_NORM(u::Array{<:Unitful.AbstractQuantity, N},
                                              t) where {N}
     sqrt(sum(x -> DiffEqBase.ODE_DEFAULT_NORM(x[1], x[2]),
-             zip((DiffEEqBase.value(x) for x in u), Iterators.repeated(t))) / length(u))
+             zip((value(x) for x in u), Iterators.repeated(t))) / length(u))
 end
 @inline DiffEqBase.ODE_DEFAULT_NORM(u::Unitful.AbstractQuantity, t) = abs(value(u))
 @inline function DiffEqBase.UNITLESS_ABS2(x::Unitful.AbstractQuantity)
