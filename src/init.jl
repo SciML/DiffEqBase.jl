38b70e4000d90060e71b06c09dc0ef0e4020749b
Chris Rackauckas <accounts@chrisrackauckas.com>
2023-02-06 18:27:38 -0500

    format and don't define init unless required (get it?)

--- src/init.jl
+++ src/init.jl
@@ -14,8 +14,8 @@ function SciMLBase.tmap(args...)
     error("Zygote must be added to differentiate Zygote? If you see this error, report it.")
 end
 
-function __init__()
-    @static if !isdefined(Base, :get_extension)
+@static if !isdefined(Base, :get_extension)
+    function __init__()
         @require Measurements="eff96d63-e80a-5855-80a2-b1b0885c5ab7" begin include("../ext/DiffEqBaseMeasurementsExt.jl") end
 
         @require MonteCarloMeasurements="0987c9cc-fe09-11e8-30f0-b96dd679fdca" begin include("../ext/DiffEqBaseMonteCarloMeasurementsExt.jl") end
@@ -29,7 +29,7 @@ function __init__()
         @require ReverseDiff="37e2e3b7-166d-5795-8a7a-e32c996b4267" begin include("../ext/DiffEqBaseReverseDiffExt.jl") end
 
         @require Zygote="e88e6eb3-aa80-5325-afca-941959d7151f" begin include("../ext/DiffEqBaseZygoteExt.jl") end
-        
+
         @require MPI="da04e1cc-30fd-572f-bb4f-1f8673147195" begin include("../ext/DiffEqBaseMPIExt.jl") end
     end
 end
