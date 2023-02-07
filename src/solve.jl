b043fcf25a461d66cede4cae62b0b26fbfd6ccd0
Oscar Smith <oscardssmith@gmail.com>
2023-02-07 09:25:15 -0500

    fix test failures

--- src/solve.jl
+++ src/solve.jl
@@ -418,7 +418,7 @@ function init_call(_prob, args...; merge_callbacks = true, kwargshandle = Keywor
 
     checkkwargs(kwargshandle; kwargs...)
 
-    if _prob isa DEProblem && isnothing(_prob.u0)
+    if _prob isa DEProblem && isdefined(_prob, :u0) &&isnothing(_prob.u0)
         build_null_integrator(_prob, args...; kwargs...)
     elseif hasfield(typeof(_prob), :f) && hasfield(typeof(_prob.f), :f) &&
            typeof(_prob.f.f) <: EvalFunc
