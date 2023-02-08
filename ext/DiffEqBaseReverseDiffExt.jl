eacfc9c909dc4610daa4fea4100b0aba3226b26c
Chris Rackauckas <accounts@chrisrackauckas.com>
2023-02-08 01:24:05 -0500

    Remove noise pieces

--- ext/DiffEqBaseReverseDiffExt.jl
+++ ext/DiffEqBaseReverseDiffExt.jl
@@ -116,22 +116,6 @@ function DiffEqBase.solve_up(prob::DiffEqBase.DEProblem,
     DiffEqBase.solve_up(prob, sensealg, reduce(vcat, u0), p, args...; kwargs...)
 end
 
-@inline function DiffEqNoiseProcess.wiener_randn(rng::Random.AbstractRNG,
-                                                 proto::ReverseDiff.TrackedArray)
-    ReverseDiff.track(convert.(eltype(proto.value), randn(rng, size(proto))))
-end
-@inline function DiffEqNoiseProcess.wiener_randn!(rng::AbstractRNG,
-                                                  rand_vec::Array{<:ReverseDiff.TrackedReal
-                                                                  })
-    rand_vec .= ReverseDiff.track.(randn.((rng,), typeof.(DiffEqBase.value.(rand_vec))))
-end
-@inline function DiffEqNoiseProcess.wiener_randn!(rng::AbstractRNG,
-                                                  rand_vec::AbstractArray{
-                                                                          <:ReverseDiff.TrackedReal
-                                                                          })
-    rand_vec .= ReverseDiff.track.(randn.((rng,), typeof.(DiffEqBase.value.(rand_vec))))
-end
-
 # Required becase ReverseDiff.@grad function DiffEqBase.solve_up is not supported!
 import DiffEqBase: solve_up
 ReverseDiff.@grad function solve_up(prob, sensealg, u0, p, args...; kwargs...)
