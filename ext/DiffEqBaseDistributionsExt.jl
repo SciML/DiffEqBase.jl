080ed85350dca0b7404134981120a1685a4362ad
Christopher Rackauckas <accounts@chrisrackauckas.com>
2023-03-06 08:07:08 -0500

    Update ext/DiffEqBaseDistributionsExt.jl

--- ext/DiffEqBaseDistributionsExt.jl
+++ ext/DiffEqBaseDistributionsExt.jl
@@ -1,12 +1,6 @@
 module DiffEqBaseDistributionsExt
 
-if isdefined(Base, :get_extension)
-    using Distributions
-    using DiffEqBase
-else
-    using ..Distributions
-    using ..DiffEqBase
-end
+using Distributions, DiffEqBase
 
 DiffEqBase.handle_distribution_u0(_u0::Distributions.Sampleable) = rand(_u0)
 DiffEqBase.isdistribution(_u0::Distributions.Sampleable) = true
