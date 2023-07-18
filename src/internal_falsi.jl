3e68051127d650b6e7d025616ec3d3522d3cb6ad
Christopher Rackauckas <accounts@chrisrackauckas.com>
2023-07-18 13:27:08 -0400

    Update internal_falsi.jl

--- src/internal_falsi.jl
+++ src/internal_falsi.jl
@@ -93,3 +93,60 @@ function SciMLBase.solve(prob::IntervalNonlinearProblem, alg::InternalFalsi, arg
     return SciMLBase.build_solution(prob, alg, left, fl; retcode = ReturnCode.MaxIters,
         left = left, right = right)
 end
+
+function scalar_nlsolve_ad(prob, alg::InternalFalsi, args...; kwargs...)
+    f = prob.f
+    p = value(prob.p)
+
+    if prob isa IntervalNonlinearProblem
+        tspan = value(prob.tspan)
+        newprob = IntervalNonlinearProblem(f, tspan, p; prob.kwargs...)
+    else
+        u0 = value(prob.u0)
+        newprob = NonlinearProblem(f, u0, p; prob.kwargs...)
+    end
+
+    sol = solve(newprob, alg, args...; kwargs...)
+
+    uu = sol.u
+    if p isa Number
+        f_p = ForwardDiff.derivative(Base.Fix1(f, uu), p)
+    else
+        f_p = ForwardDiff.gradient(Base.Fix1(f, uu), p)
+    end
+
+    f_x = ForwardDiff.derivative(Base.Fix2(f, p), uu)
+    pp = prob.p
+    sumfun = let f_x′ = -f_x
+        ((fp, p),) -> (fp / f_x′) * ForwardDiff.partials(p)
+    end
+    partials = sum(sumfun, zip(f_p, pp))
+    return sol, partials
+end
+
+function SciMLBase.solve(prob::IntervalNonlinearProblem{uType, iip,
+    <:ForwardDiff.Dual{T, V, P}},
+    alg::InteralFalsi, args...;
+    kwargs...) where {uType, iip, T, V, P}
+    sol, partials = scalar_nlsolve_ad(prob, alg, args...; kwargs...)
+    return SciMLBase.build_solution(prob, alg, Dual{T, V, P}(sol.u, partials),
+        sol.resid; retcode = sol.retcode,
+        left = Dual{T, V, P}(sol.left, partials),
+        right = Dual{T, V, P}(sol.right, partials))
+end
+
+function SciMLBase.solve(prob::IntervalNonlinearProblem{uType, iip,
+    <:AbstractArray{
+        <:ForwardDiff.Dual{T,
+            V,
+            P},
+    }},
+    alg::InteralFalsi, args...;
+    kwargs...) where {uType, iip, T, V, P}
+    sol, partials = scalar_nlsolve_ad(prob, alg, args...; kwargs...)
+    
+    return SciMLBase.build_solution(prob, alg, Dual{T, V, P}(sol.u, partials),
+        sol.resid; retcode = sol.retcode,
+        left = Dual{T, V, P}(sol.left, partials),
+        right = Dual{T, V, P}(sol.right, partials))
+end
