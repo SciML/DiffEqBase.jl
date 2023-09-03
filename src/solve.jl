4cfa2aa913f8bbc1b174b4696d00c253bfc22e62
Chris Rackauckas <accounts@chrisrackauckas.com>
2023-09-03 10:54:09 -0400

    typo

--- src/solve.jl
+++ src/solve.jl
@@ -1306,9 +1306,7 @@ function check_prob_alg_pairing(prob, alg)
         throw(DirectAutodiffError())
     end
 
-    @show "here?"
-
-    if prob isa SDEProblem && noise_rate_prototype !== nothing &&
+    if prob isa SDEProblem && prob.noise_rate_prototype !== nothing &&
         prob.noise !== nothing && size(prob.noise_rate_prototype,2) != length(prob.noise.W[1])
         throw(NoiseSizeIncompatabilityError(size(prob.noise_rate_prototype,2), length(prob.noise.W[1])))
     end
