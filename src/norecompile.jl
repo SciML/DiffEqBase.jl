e9a5bc77744d5672337b33a6415451b44b150047
oscarddssmith <oscar.smith@juliacomputing.com>
2023-11-03 14:51:32 -0400

    dualT4 should not be promoted with T since T might already be a dual

--- src/norecompile.jl
+++ src/norecompile.jl
@@ -57,7 +57,7 @@ function wrapfun_iip(ff,
     dualT = dualgen(T)
     dualT1 = ArrayInterface.promote_eltype(T1, dualT)
     dualT2 = ArrayInterface.promote_eltype(T2, dualT)
-    dualT4 = dualgen(promote_type(T, T4))
+    dualT4 = dualgen(T4)
 
     iip_arglists = (Tuple{T1, T2, T3, T4},
         Tuple{dualT1, dualT2, T3, T4},
