fc5eab027ea920eab5c06051051b6484aefc4654
Aayush Sabharwal <aayush.sabharwal@juliahub.com>
2023-12-18 11:47:15 +0530

    fix: fix eltype checking bug

--- src/solve.jl
+++ src/solve.jl
@@ -588,7 +588,7 @@ function solve_call(_prob, args...; merge_callbacks = true, kwargshandle = nothi
                 throw(NonConcreteEltypeError(RecursiveArrayTools.recursive_unitless_eltype(_prob.u0)))
             end
 
-            if !(eltype(_prob.u0) <: Number) && !(eltype(_prob.u0) <: Enum)
+            if !(RecursiveArrayTools.recursive_unitless_bottom_eltype(_prob.u0) <: Number) && !(RecursiveArrayTools.recursive_unitless_bottom_eltype(_prob.u0) <: Enum)
                 # Allow Enums for FunctionMaps, make into a trait in the future
                 throw(NonNumberEltypeError(eltype(_prob.u0)))
             end
