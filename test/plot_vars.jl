dfd5d83ff60d3b03bea23840d95ea35e6991f19d
Christopher Rackauckas <accounts@chrisrackauckas.com>
2023-12-27 16:10:16 -0500

    Update plot vars tests

--- test/plot_vars.jl
+++ test/plot_vars.jl
@@ -18,58 +18,39 @@ dt = 0.1
 sol = solve(prob, InternalEuler.FwdEulerAlg(), tstops = 0:dt:1)
 syms = [:x, :y, :z]
 
-@test SciMLBase.interpret_vars([(0, 1), (1, 3), (4, 5)], sol, syms) == [
+@test SciMLBase.interpret_vars([(0, 1), (1, 3), (4, 5)], sol) == [
     (SciMLBase.DEFAULT_PLOT_FUNC, 0, 1),
     (SciMLBase.DEFAULT_PLOT_FUNC, 1, 3),
     (SciMLBase.DEFAULT_PLOT_FUNC, 4, 5),
 ]
-@test SciMLBase.interpret_vars([1, (1, 3), (4, 5)], sol, syms) == [
+@test SciMLBase.interpret_vars([1, (1, 3), (4, 5)], sol) == [
     (SciMLBase.DEFAULT_PLOT_FUNC, 0, 1),
     (SciMLBase.DEFAULT_PLOT_FUNC, 1, 3),
     (SciMLBase.DEFAULT_PLOT_FUNC, 4, 5),
 ]
-@test SciMLBase.interpret_vars([1, 3, 4], sol, syms) == [
+@test SciMLBase.interpret_vars([1, 3, 4], sol) == [
     (SciMLBase.DEFAULT_PLOT_FUNC, 0, 1),
     (SciMLBase.DEFAULT_PLOT_FUNC, 0, 3),
     (SciMLBase.DEFAULT_PLOT_FUNC, 0, 4),
 ]
-@test SciMLBase.interpret_vars(([1, 2, 3], [4, 5, 6]), sol, syms) == [
+@test SciMLBase.interpret_vars(([1, 2, 3], [4, 5, 6]), sol) == [
     (SciMLBase.DEFAULT_PLOT_FUNC, 1, 4),
     (SciMLBase.DEFAULT_PLOT_FUNC, 2, 5),
     (SciMLBase.DEFAULT_PLOT_FUNC, 3, 6),
 ]
-@test SciMLBase.interpret_vars((1, [2, 3, 4]), sol, syms) == [
+@test SciMLBase.interpret_vars((1, [2, 3, 4]), sol) == [
     (SciMLBase.DEFAULT_PLOT_FUNC, 1, 2),
     (SciMLBase.DEFAULT_PLOT_FUNC, 1, 3),
     (SciMLBase.DEFAULT_PLOT_FUNC, 1, 4),
 ]
 
-@test SciMLBase.interpret_vars([(:t, :x), (:t, :y)], sol, syms) ==
-      [(SciMLBase.DEFAULT_PLOT_FUNC, 0, 1), (SciMLBase.DEFAULT_PLOT_FUNC, 0, 2)]
-@test SciMLBase.interpret_vars([:x, (0, :x), (:x, :y)], sol, syms) == [
-    (SciMLBase.DEFAULT_PLOT_FUNC, 0, 1),
-    (SciMLBase.DEFAULT_PLOT_FUNC, 0, 1),
-    (SciMLBase.DEFAULT_PLOT_FUNC, 1, 2),
-]
-@test SciMLBase.interpret_vars([:x, :y, :z], sol, syms) == [
-    (SciMLBase.DEFAULT_PLOT_FUNC, 0, 1),
-    (SciMLBase.DEFAULT_PLOT_FUNC, 0, 2),
-    (SciMLBase.DEFAULT_PLOT_FUNC, 0, 3),
-]
-@test SciMLBase.interpret_vars(([:x, :x], [:y, :z]), sol, syms) ==
-      [(SciMLBase.DEFAULT_PLOT_FUNC, 1, 2), (SciMLBase.DEFAULT_PLOT_FUNC, 1, 3)]
-@test SciMLBase.interpret_vars((:x, [:y, :z]), sol, syms) ==
-      [(SciMLBase.DEFAULT_PLOT_FUNC, 1, 2), (SciMLBase.DEFAULT_PLOT_FUNC, 1, 3)]
-
 f(x, y) = (x + y, y)
-@test SciMLBase.interpret_vars([(f, 0, 1), (1, 3), (4, 5)], sol, syms) ==
+@test SciMLBase.interpret_vars([(f, 0, 1), (1, 3), (4, 5)], sol) ==
       [(f, 0, 1), (SciMLBase.DEFAULT_PLOT_FUNC, 1, 3), (SciMLBase.DEFAULT_PLOT_FUNC, 4, 5)]
-@test SciMLBase.interpret_vars([1, (f, 1, 3), (4, 5)], sol, syms) ==
+@test SciMLBase.interpret_vars([1, (f, 1, 3), (4, 5)], sol) ==
       [(SciMLBase.DEFAULT_PLOT_FUNC, 0, 1), (f, 1, 3), (SciMLBase.DEFAULT_PLOT_FUNC, 4, 5)]
-@test SciMLBase.interpret_vars([(f, :t, :x), (:t, :y)], sol, syms) ==
-      [(f, 0, 1), (SciMLBase.DEFAULT_PLOT_FUNC, 0, 2)]
-@test SciMLBase.interpret_vars([:x, (f, 0, :x), (:x, :y)], sol, syms) ==
+@test SciMLBase.interpret_vars([1, (f, 0, 1), (1, 2)], sol) ==
       [(SciMLBase.DEFAULT_PLOT_FUNC, 0, 1), (f, 0, 1), (SciMLBase.DEFAULT_PLOT_FUNC, 1, 2)]
-@test SciMLBase.interpret_vars([(:x, :y)], sol, syms) ==
+@test SciMLBase.interpret_vars([(1, 2)], sol) ==
       [(SciMLBase.DEFAULT_PLOT_FUNC, 1, 2)]
-@test SciMLBase.interpret_vars((f, :x, :y), sol, syms) == [(f, 1, 2)]
+@test SciMLBase.interpret_vars((f, 1, 2), sol) == [(f, 1, 2)]
