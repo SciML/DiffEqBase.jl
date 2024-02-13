6f9e7734131e9e5b6d387bee52896701e831093e
Aayush Sabharwal <aayush.sabharwal@juliahub.com>
2024-02-13 15:12:14 +0530

    fix: edge case when SciMLFunction.mass_matrix does not exist

--- src/callbacks.jl
+++ src/callbacks.jl
@@ -603,7 +603,8 @@ end
         end
         integrator.u_modified = true
         callback.affect!(integrator)
-        if integrator.sol.prob isa DAEProblem || !(integrator.f.mass_matrix isa UniformScaling)
+        if integrator.sol.prob isa DAEProblem || SciMLBase.__has_mass_matrix(integrator.f) &&
+            !(integrator.f.mass_matrix isa UniformScaling)
             DiffEqBase.initialize_dae!(integrator)
         end
         @inbounds if callback.save_positions[2]
