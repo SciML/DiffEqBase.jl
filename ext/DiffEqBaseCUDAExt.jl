7463267ee5055497ca7f378c7a00974f960f750c
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-05-20 00:30:34 -0400

    Update DiffEqBaseCUDAExt.jl

--- ext/DiffEqBaseCUDAExt.jl
+++ ext/DiffEqBaseCUDAExt.jl
@@ -3,7 +3,7 @@ module DiffEqBaseCUDAExt
 using DiffEqBase, CUDA
 
 function DiffEqBase.ODE_DEFAULT_NORM(u::CuArray{T},t) where {T <: Union{AbstractFloat, Complex}}
-    sqrt(sum(DiffEqBase.sse, u; init = DiffEqBase.sse(zero(T))) / totallength(u))
+    sqrt(sum(DiffEqBase.sse, u; init = DiffEqBase.sse(zero(T))) / DiffEqBase.totallength(u))
 end
 
 end
