44735a5343a708dbe548a12c79fd2c93d6786c80
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-05-20 00:04:05 -0400

    Make sure to extend

--- ext/DiffEqBaseCUDAExt.jl
+++ ext/DiffEqBaseCUDAExt.jl
@@ -2,7 +2,7 @@ module DiffEqBaseCUDAExt
 
 using DiffEqBase, CUDA
 
-function ODE_DEFAULT_NORM(u::CuArray{T},t) where {T <: Union{AbstractFloat, Complex}}
+function DiffEqBase.ODE_DEFAULT_NORM(u::CuArray{T},t) where {T <: Union{AbstractFloat, Complex}}
     sqrt(sum(DiffEqBase.sse, u; init = DiffEqBase.sse(zero(T))) / totallength(u))
 end
 
