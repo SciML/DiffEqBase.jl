0dd4f13fdf5dbd265e1ec56d712d155e364b9a00
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-07-31 19:03:07 -0400

    Update tables.jl

--- test/downstream/tables.jl
+++ test/downstream/tables.jl
@@ -1,11 +1,11 @@
-using OrdinaryDiffEq, DataFrames, Test
+using OrdinaryDiffEq, DataFrames, Test, SymbolicIndexingInterface
 f_2dlinear = (du, u, p, t) -> du .= 1.01u;
 prob = ODEProblem(f_2dlinear, rand(2, 2), (0.0, 1.0));
 sol1 = solve(prob, Euler(); dt = 1 // 2^(4));
 df = DataFrame(sol1)
 @test names(df) == ["timestamp", "value1", "value2", "value3", "value4"]
 
-prob = ODEProblem(ODEFunction(f_2dlinear, syms = [:a, :b, :c, :d]), rand(2, 2), (0.0, 1.0));
+prob = ODEProblem(ODEFunction(f_2dlinear, sys = SymbolicIndexingInterface.SymbolCache(syms, paramsyms, indepsym), rand(2, 2), (0.0, 1.0));
 sol2 = solve(prob, Euler(); dt = 1 // 2^(4));
 df = DataFrame(sol2)
 @test names(df) == ["timestamp", "a", "b", "c", "d"]
