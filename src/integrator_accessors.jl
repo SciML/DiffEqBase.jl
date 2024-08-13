c2702682779cab218ceed89b7f672e74875071df
Sam Isaacson <isaacsas@users.noreply.github.com>
2024-08-13 14:03:18 -0400

    short type info in error message

--- src/integrator_accessors.jl
+++ src/integrator_accessors.jl
@@ -2,8 +2,8 @@
 # StochasticDiffEq and provide dispatch points that JumpProcesses and others can use.
 
 get_tstops(integ::DEIntegrator) =
-    error("get_tstops not implemented for integrators of type $(typeof(integ))")
+    error("get_tstops not implemented for integrators of type $(nameof(typeof(integ)))")
 get_tstops_array(integ::DEIntegrator) =
-    error("get_tstops_array not implemented for integrators of type $(typeof(integ))")
+    error("get_tstops_array not implemented for integrators of type $(nameof(typeof(integ)))")
 get_tstops_max(integ::DEIntegrator) =
-    error("get_tstops_max not implemented for integrators of type $(typeof(integ))")
+    error("get_tstops_max not implemented for integrators of type $(nameof(typeof(integ)))")
