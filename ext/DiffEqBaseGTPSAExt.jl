e76352bb0a5fbaf53ef4640f00cf0763bc15cd7b
Matt Signorelli <mgs255@cornell.edu>
2025-01-04 18:59:07 -0500

    add ODE_DEFAULT_NORM overloads

--- ext/DiffEqBaseGTPSAExt.jl
+++ ext/DiffEqBaseGTPSAExt.jl
@@ -2,16 +2,26 @@ module DiffEqBaseGTPSAExt
 
 if isdefined(Base, :get_extension)
     using DiffEqBase
-    import DiffEqBase: value
+    import DiffEqBase: value, ODE_DEFAULT_NORM
     using GTPSA
 else
     using ..DiffEqBase
-    import ..DiffEqBase: value
+    import ..DiffEqBase: value, ODE_DEFAULT_NORM
     using ..GTPSA
 end
 
-value(x::TPS) = scalar(x);
+value(x::TPS) = scalar(x)
 value(::Type{TPS{T}}) where {T} = T
 
+ODE_DEFAULT_NORM(u::TPS, t) = @fastmath abs(value(u))
+ODE_DEFAULT_NORM(f::F, u::TPS, t) where {F} = @fastmath abs(f(value(u)))
+
+function ODE_DEFAULT_NORM(u::AbstractArray{TPS{T}}, t) where {T <: Union{AbstractFloat, Complex}}
+    x = zero(real(T))
+    @inbounds @fastmath for ui in u
+        x += abs2(value(ui))
+    end
+    Base.FastMath.sqrt_fast(x / max(length(u), 1))
+end
 
 end
