0f8481cdb83740d52c6ead858097edeca58f4e10
Matt Signorelli <mgs255@cornell.edu>
2025-01-05 09:37:06 -0500

    value -> normTPS

--- ext/DiffEqBaseGTPSAExt.jl
+++ ext/DiffEqBaseGTPSAExt.jl
@@ -13,13 +13,21 @@ end
 value(x::TPS) = scalar(x)
 value(::Type{TPS{T}}) where {T} = T
 
-ODE_DEFAULT_NORM(u::TPS, t) = @fastmath abs(value(u))
-ODE_DEFAULT_NORM(f::F, u::TPS, t) where {F} = @fastmath abs(f(value(u)))
+ODE_DEFAULT_NORM(u::TPS, t) = normTPS(u)
+ODE_DEFAULT_NORM(f::F, u::TPS, t) where {F} = normTPS(f(u))
 
-function ODE_DEFAULT_NORM(u::AbstractArray{TPS{T}}, t) where {T <: Union{AbstractFloat, Complex}}
+function ODE_DEFAULT_NORM(u::AbstractArray{TPS{T}}, t) where {T}
     x = zero(real(T))
     @inbounds @fastmath for ui in u
-        x += abs2(value(ui))
+        x += normTPS(ui)^2
+    end
+    Base.FastMath.sqrt_fast(x / max(length(u), 1))
+end
+
+function ODE_DEFAULT_NORM(f::F, u::AbstractArray{TPS{T}}, t) where {F, T}
+    x = zero(real(T))
+    @inbounds @fastmath for ui in u
+        x += normTPS(f(ui))^2
     end
     Base.FastMath.sqrt_fast(x / max(length(u), 1))
 end
