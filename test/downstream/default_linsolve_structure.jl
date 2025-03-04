9dba5a4abbabe49ee99f47283b20ce2285048234
David Widmann <devmotion@users.noreply.github.com>
2025-03-04 09:51:43 +0100

    Use ADTypes in tests

--- test/downstream/default_linsolve_structure.jl
+++ test/downstream/default_linsolve_structure.jl
@@ -1,4 +1,4 @@
-using LinearAlgebra, OrdinaryDiffEq, Test
+using LinearAlgebra, OrdinaryDiffEq, Test, ADTypes
 f = (du, u, p, t) -> du .= u ./ t
 jac = (J, u, p, t) -> (J[1, 1] = 1 / t; J[2, 2] = 1 / t; J[1, 2] = 0; J[2, 1] = 0)
 
@@ -9,7 +9,7 @@ sol = solve(prob, Rosenbrock23())
 @test sol.u[end] ≈ [10.0, 10.0]
 @test length(sol) < 60
 
-sol = solve(prob, Rosenbrock23(autodiff = false))
+sol = solve(prob, Rosenbrock23(autodiff = AutoFiniteDiff()))
 @test sol.u[end] ≈ [10.0, 10.0]
 @test length(sol) < 60
 
@@ -21,7 +21,7 @@ sol = solve(prob, Rosenbrock23())
 @test sol.u[end] ≈ [10.0, 10.0]
 @test length(sol) < 60
 
-sol = solve(prob, Rosenbrock23(autodiff = false))
+sol = solve(prob, Rosenbrock23(autodiff = AutoFiniteDiff()))
 @test sol.u[end] ≈ [10.0, 10.0]
 @test length(sol) < 60
 
@@ -42,7 +42,7 @@ sol = solve(prob,Rosenbrock23())
     jp = Hermitian(jp_diag)
     fun = ODEFunction(f; jac = jac, jac_prototype = jp)
     prob = ODEProblem(fun, ones(2), (1.0, 10.0))
-    sol = solve(prob, Rosenbrock23(autodiff = false))
+    sol = solve(prob, Rosenbrock23(autodiff = AutoFiniteDiff()))
     @test sol.u[end] ≈ [10.0, 10.0]
     @test length(sol) < 60
 end
@@ -51,7 +51,7 @@ end
     jp = Symmetric(jp_diag)
     fun = ODEFunction(f; jac = jac, jac_prototype = jp)
     prob = ODEProblem(fun, ones(2), (1.0, 10.0))
-    sol = solve(prob, Rosenbrock23(autodiff = false))
+    sol = solve(prob, Rosenbrock23(autodiff = AutoFiniteDiff()))
     @test sol.u[end] ≈ [10.0, 10.0]
     @test length(sol) < 60
 end
