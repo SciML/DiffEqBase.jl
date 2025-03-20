897df65a00582257ae764673c5bf93a94552b4b6
Chris Rackauckas <accounts@chrisrackauckas.com>
2025-03-20 13:09:36 -0100

    Remove termination conditions tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -35,7 +35,6 @@ end
         @time @safetestset "ForwardDiff Dual Detection" include("forwarddiff_dual_detection.jl")
         @time @safetestset "ODE default norm" include("ode_default_norm.jl")
         @time @safetestset "ODE default unstable check" include("ode_default_unstable_check.jl")
-        @time @safetestset "Termination Conditions" include("termination_conditions.jl")
     end
 
     if !is_APPVEYOR && GROUP == "Downstream"
