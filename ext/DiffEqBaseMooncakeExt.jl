f36d93c54e6d58e6c68d847d5def53fb494897ab
Christopher Rackauckas <accounts@chrisrackauckas.com>
2025-05-24 01:06:32 +0000

    add auto-alg dispatch

--- ext/DiffEqBaseMooncakeExt.jl
+++ ext/DiffEqBaseMooncakeExt.jl
@@ -16,6 +16,19 @@ Mooncake.@from_rrule(
     true,
     )
 
+# Dispatch for auto-alg
+Mooncake.@from_rrule(
+    Mooncake.MinimalCtx,
+    Tuple{
+        typeof(DiffEqBase.solve_up),
+        DiffEqBase.AbstractDEProblem,
+        Union{Nothing, DiffEqBase.AbstractSensitivityAlgorithm},
+        Any,
+        Any,
+    },
+    true,
+    )
+
 Mooncake.@zero_adjoint Mooncake.MinimalCtx Tuple{typeof(DiffEqBase.numargs), Any}
 Mooncake.@mooncake_overlay DiffEqBase.set_mooncakeoriginator_if_mooncake(x::ADOriginator) = MooncakeOriginator
 
