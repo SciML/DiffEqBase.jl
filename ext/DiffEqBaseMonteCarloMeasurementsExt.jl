ed05d57f3769ecc6817ed5f3444b750ec40b16b3
jClugstor <jadonclugston@gmail.com>
2025-08-18 12:36:27 -0400

    excise extension things that are now in SciMLBase

--- ext/DiffEqBaseMonteCarloMeasurementsExt.jl
+++ ext/DiffEqBaseMonteCarloMeasurementsExt.jl
@@ -4,34 +4,6 @@ using DiffEqBase
 import DiffEqBase: value
 using MonteCarloMeasurements
 
-function DiffEqBase.promote_u0(
-        u0::AbstractArray{
-            <:MonteCarloMeasurements.AbstractParticles,
-        },
-        p::AbstractArray{<:MonteCarloMeasurements.AbstractParticles},
-        t0)
-    u0
-end
-function DiffEqBase.promote_u0(u0,
-        p::AbstractArray{<:MonteCarloMeasurements.AbstractParticles},
-        t0)
-    eltype(p).(u0)
-end
-
-function DiffEqBase.promote_u0(::Nothing,
-        p::AbstractArray{<:MonteCarloMeasurements.AbstractParticles},
-        t0)
-    return nothing
-end
-
-DiffEqBase.value(x::Type{MonteCarloMeasurements.AbstractParticles{T, N}}) where {T, N} = T
-DiffEqBase.value(x::MonteCarloMeasurements.AbstractParticles) = mean(x.particles)
-function DiffEqBase.unitfulvalue(x::Type{MonteCarloMeasurements.AbstractParticles{
-        T, N}}) where {T, N}
-    T
-end
-DiffEqBase.unitfulvalue(x::MonteCarloMeasurements.AbstractParticles) = mean(x.particles)
-
 # Support adaptive steps should be errorless
 @inline function DiffEqBase.ODE_DEFAULT_NORM(
         u::AbstractArray{
--- ext/DiffEqBaseTrackerExt.jl
+++ ext/DiffEqBaseTrackerExt.jl
@@ -4,36 +4,6 @@ using DiffEqBase
 import DiffEqBase: value
 import Tracker
 
-DiffEqBase.value(x::Type{Tracker.TrackedReal{T}}) where {T} = T
-DiffEqBase.value(x::Type{Tracker.TrackedArray{T, N, A}}) where {T, N, A} = Array{T, N}
-DiffEqBase.value(x::Tracker.TrackedReal) = x.data
-DiffEqBase.value(x::Tracker.TrackedArray) = x.data
-
-DiffEqBase.unitfulvalue(x::Type{Tracker.TrackedReal{T}}) where {T} = T
-function DiffEqBase.unitfulvalue(x::Type{Tracker.TrackedArray{T, N, A}}) where {T, N, A}
-    Array{T, N}
-end
-DiffEqBase.unitfulvalue(x::Tracker.TrackedReal) = x.data
-DiffEqBase.unitfulvalue(x::Tracker.TrackedArray) = x.data
-
-DiffEqBase.promote_u0(u0::Tracker.TrackedArray, p::Tracker.TrackedArray, t0) = u0
-function DiffEqBase.promote_u0(u0::AbstractArray{<:Tracker.TrackedReal},
-        p::Tracker.TrackedArray, t0)
-    u0
-end
-function DiffEqBase.promote_u0(u0::Tracker.TrackedArray,
-        p::AbstractArray{<:Tracker.TrackedReal}, t0)
-    u0
-end
-function DiffEqBase.promote_u0(u0::AbstractArray{<:Tracker.TrackedReal},
-        p::AbstractArray{<:Tracker.TrackedReal}, t0)
-    u0
-end
-DiffEqBase.promote_u0(u0, p::Tracker.TrackedArray, t0) = Tracker.track(u0)
-DiffEqBase.promote_u0(u0, p::AbstractArray{<:Tracker.TrackedReal}, t0) = eltype(p).(u0)
-
-@inline Base.any(f::Function, x::Tracker.TrackedArray) = any(f, Tracker.data(x))
-
 # Support adaptive with non-tracked time
 @inline function DiffEqBase.ODE_DEFAULT_NORM(u::Tracker.TrackedArray, t)
     sqrt(sum(abs2, DiffEqBase.value(u)) / length(u))
