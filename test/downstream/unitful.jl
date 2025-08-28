a843902f02643a831f38c656db419a1191d76fa4
jClugstor <jadonclugston@gmail.com>
2025-08-28 12:43:00 -0400

    fix unitful tests

--- test/downstream/unitful.jl
+++ test/downstream/unitful.jl
@@ -4,6 +4,6 @@ prob = ODEProblem(f, [2.0u"m"], (0.0u"s", Inf * u"s"))
 intg = init(prob, Tsit5())
 @test_nowarn step!(intg, 0.02u"s", true)
 
-@test DiffEqBase.unitfulvalue(u"1/s") == u"1/s"
-@test DiffEqBase.value(ForwardDiff.Dual(1) * u"1/s") == 1
-@test DiffEqBase.unitfulvalue(ForwardDiff.Dual(1) * u"1/s") == u"1/s"
+@test SciMLBase.unitfulvalue(u"1/s") == u"1/s"
+@test SciMLBase.value(ForwardDiff.Dual(1) * u"1/s") == 1
+@test SciMLBase.unitfulvalue(ForwardDiff.Dual(1) * u"1/s") == u"1/s"
