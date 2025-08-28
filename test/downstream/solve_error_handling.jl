8b82b83a2932062ca523df3ae4a01c5c0fafc597
jClugstor <jadonclugston@gmail.com>
2025-08-28 12:09:15 -0400

    change errors to reference SciMLBase

--- test/downstream/solve_error_handling.jl
+++ test/downstream/solve_error_handling.jl
@@ -10,7 +10,7 @@ function f(du, u, p, t)
     du .= 2.0 * u
 end
 prob = ODEProblem(f, u0, tspan)
-@test_throws DiffEqBase.IncompatibleInitialConditionError sol=solve(prob, Tsit5())
+@test_throws SciMLBase.IncompatibleInitialConditionError sol=solve(prob, Tsit5())
 
 prob = ODEProblem{false}(f, u0, tspan)
 sol = solve(prob, Tsit5())
@@ -18,40 +18,40 @@ sol = solve(prob, nothing, alg = Tsit5())
 sol = init(prob, nothing, alg = Tsit5())
 
 prob = ODEProblem{false}(f, 1.0 + im, tspan)
-@test_throws DiffEqBase.ComplexSupportError solve(prob, CVODE_Adams())
+@test_throws SciMLBase.ComplexSupportError solve(prob, CVODE_Adams())
 
-@test_throws DiffEqBase.ProblemSolverPairingError solve(prob, DFBDF())
-@test_throws DiffEqBase.NonSolverError solve(prob, 5.0)
+@test_throws SciMLBase.ProblemSolverPairingError solve(prob, DFBDF())
+@test_throws SciMLBase.NonSolverError solve(prob, 5.0)
 
 prob = ODEProblem{false}(f, u0, (nothing, nothing))
-@test_throws DiffEqBase.NoTspanError solve(prob, Tsit5())
+@test_throws SciMLBase.NoTspanError solve(prob, Tsit5())
 
 prob = ODEProblem{false}(f, u0, (NaN, 1.0))
-@test_throws DiffEqBase.NaNTspanError solve(prob, Tsit5())
+@test_throws SciMLBase.NaNTspanError solve(prob, Tsit5())
 
 prob = ODEProblem{false}(f, u0, (1.0, NaN))
-@test_throws DiffEqBase.NaNTspanError solve(prob, Tsit5())
+@test_throws SciMLBase.NaNTspanError solve(prob, Tsit5())
 
 prob = ODEProblem{false}(f, Any[1.0, 1.0f0], tspan)
-@test_throws DiffEqBase.NonConcreteEltypeError solve(prob, Tsit5())
+@test_throws SciMLBase.NonConcreteEltypeError solve(prob, Tsit5())
 
 prob = ODEProblem{false}(f, (1.0, 1.0f0), tspan)
-@test_throws DiffEqBase.TupleStateError solve(prob, Tsit5())
+@test_throws SciMLBase.TupleStateError solve(prob, Tsit5())
 
 prob = ODEProblem{false}(f, u0, (0.0 + im, 1.0))
-@test_throws DiffEqBase.ComplexTspanError solve(prob, Tsit5())
+@test_throws SciMLBase.ComplexTspanError solve(prob, Tsit5())
 
 for u0 in ([0.0, 0.0], nothing)
     fmm = ODEFunction(f, mass_matrix = zeros(3, 3))
     prob = ODEProblem(fmm, u0, (0.0, 1.0))
-    @test_throws DiffEqBase.IncompatibleMassMatrixError solve(prob, Tsit5())
+    @test_throws SciMLBase.IncompatibleMassMatrixError solve(prob, Tsit5())
 end
 
 # Allow empty mass matrix for empty u0
 fmm = ODEFunction((du, u, t) -> nothing, mass_matrix = zeros(0, 0))
 prob = ODEProblem(fmm, nothing, (0.0, 1.0))
 sol = solve(prob, Tsit5())
-@test isa(sol, DiffEqBase.ODESolution)
+@test isa(sol, SciMLBase.ODESolution)
 
 f(du, u, p, t) = du .= 1.01u
 function g(du, u, p, t)
@@ -71,7 +71,7 @@ prob = SDEProblem(f,
     (0.0, 1.0),
     noise_rate_prototype = complex(zeros(2, 4)),
     noise = StochasticDiffEq.RealWienerProcess(0.0, zeros(3)))
-@test_throws DiffEqBase.NoiseSizeIncompatabilityError solve(prob, LambaEM())
+@test_throws SciMLBase.NoiseSizeIncompatabilityError solve(prob, LambaEM())
 
 function g!(du, u, p, t)
     du[1] .= u[1] + ones(3, 3)
@@ -79,4 +79,4 @@ function g!(du, u, p, t)
 end
 u0 = [zeros(3, 3), zeros(3, 3)]
 prob = ODEProblem(g!, u0, (0, 1.0))
-@test_throws DiffEqBase.NonNumberEltypeError solve(prob, Tsit5())
+@test_throws SciMLBase.NonNumberEltypeError solve(prob, Tsit5())
