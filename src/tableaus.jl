8487860e63b4a983b56f2b220fd50ab22cdb3d1a
KeshavVenkatesh <keshavvenkatesh211@gmail.com>
2025-12-17 22:01:32 -0800

    Interp extension for ExplicitRK Tableau

--- src/tableaus.jl
+++ src/tableaus.jl
@@ -15,14 +15,15 @@ mutable struct ExplicitRKTableau{MType <: AbstractMatrix, VType <: AbstractVecto
     adaptiveorder::Int #The lower order of the pair. Only used for adaptivity.
     fsal::Bool
     stability_size::S
+    B_interp::MType
 end
 function ExplicitRKTableau(A::MType, c::VType, α::VType, order;
         adaptiveorder = 0, αEEst = similar(α, 0),
         fsal = false, stability_size = 0.0,
-        d = similar(α, 0)) where {MType, VType}
+        d = similar(α, 0), B_interp = nothing) where {MType, VType}
     S = typeof(stability_size)
     ExplicitRKTableau{MType, VType, S}(A, c, α, αEEst, d, length(α), order, adaptiveorder,
-        fsal, stability_size)
+        fsal, stability_size, B_interp)
 end
 
 """
